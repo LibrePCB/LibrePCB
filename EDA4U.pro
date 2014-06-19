e68d5f4b11551e307f1d2daffaeb18e095584204
ubruhin <urbibruhin@bluewin.ch>
2014-06-19 15:35:09 +0200

    added project files and main.cpp

---
+++ EDA4U.pro
@@ -0,0 +1,21 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2013-02-05T16:47:16
+#
+#-------------------------------------------------
+
+QT += core widgets opengl sql xml printsupport
+
+TARGET = EDA4U
+TEMPLATE = app
+
+RESOURCES += \
+    ressources.qrc
+
+SOURCES += \
+    src/main.cpp
+
+HEADERS +=
+
+FORMS +=
+
---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+/*
+ * EDA4U - Professional EDA for everyone!
+ * Copyright (C) 2013 Urban Bruhin
+ * http://eda4u.ubruhin.ch/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+
+#include <QtCore>
+#include <QApplication>
+#include <QTranslator>
+
+/*****************************************************************************************
+ *  main()
+ ****************************************************************************************/
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    app.installTranslator(&qtTranslator);
+
+    QTranslator appTranslator;
+    appTranslator.load("translations" + QString(QDir::separator()) + "app_" + QLocale::system().name());
+    app.installTranslator(&appTranslator);
+
+    QCoreApplication::setOrganizationName("EDA4U");
+    QCoreApplication::setApplicationName("EDA4U");
+    QCoreApplication::setApplicationVersion("0.0.1");
+
+    return app.exec();
+}
