6c68b28fd7098dd0c7ed2b8fcf75304997487510
ubruhin <urbibruhin@bluewin.ch>
2015-03-01 23:54:36 +0100

    updated tool UuidGenerator

--- tools/UuidGenerator/mainwindow.h
+++ tools/UuidGenerator/mainwindow.h
@@ -17,9 +17,13 @@ class MainWindow : public QMainWindow
 
     private slots:
         void on_pushButton_clicked();
+        void on_timer_timeout();
+
+        void on_checkBox_toggled(bool checked);
 
     private:
         Ui::MainWindow *ui;
+        QTimer* timer;
 };
 
 #endif // MAINWINDOW_H
