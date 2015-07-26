67f541da55e0afaa3f710004456fcdeec79b4e65
ubruhin <urbibruhin@bluewin.ch>
2015-07-26 15:41:48 +0200

    travis-ci: build doxygen documentation for each branch

--- dev/doxygen/travis_build_doxygen.sh
+++ dev/doxygen/travis_build_doxygen.sh
@@ -2,18 +2,22 @@
 
 set -ev
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then 
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
+    echo "Build doxygen documentation for branch ${TRAVIS_BRANCH}..."
     make clean
     cd ./dev/doxygen
     doxygen Doxyfile
     git clone -b gh-pages $DOXYGEN_REPOSITORY
-    cd LibrePCB-Doxygen
+    mkdir "LibrePCB-Doxygen/${TRAVIS_BRANCH}"
+    cd "LibrePCB-Doxygen/${TRAVIS_BRANCH}"
     git rm -r *
-    cp -rf ../output/html/* .
+    cp -rf ../../output/html/* .
     git config user.name "LibrePCB-Builder"
     git config user.email "builder@librepcb.org"
     git add -A
-    git commit -m "updated doxygen documentation"
+    git commit -m "updated doxygen documentation of branch ${TRAVIS_BRANCH}"
     git push origin gh-pages
+else
+    echo "Pull request -> Do not build doxygen documentation"
 fi
 
