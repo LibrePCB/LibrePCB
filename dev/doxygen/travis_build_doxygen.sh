b21716cbd7a4b28d0cc5be463021b81353a1af8c
ubruhin <urbibruhin@bluewin.ch>
2015-07-26 15:18:14 +0200

    updated travis-ci config

--- dev/doxygen/travis_build_doxygen.sh
+++ dev/doxygen/travis_build_doxygen.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-doxygen Doxyfile
-cd output/html
-git clone -b gh-pages $DOXYGEN_REPOSITORY
-cp -rf LibrePCB-Doxygen/.git .git
-rm -rf LibrePCB-Doxygen
-git config user.name "LibrePCB-Builder"
-git config user.email "builder@librepcb.org"
-git add --all
-git commit --all -m "updated doxygen documentation"
-git push origin gh-pages
+set -ev
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_BRANCH}" = "master" ]; then 
+    make clean
+    cd ./dev/doxygen
+    doxygen Doxyfile
+    git clone -b gh-pages $DOXYGEN_REPOSITORY
+    cd LibrePCB-Doxygen
+    git rm -r *
+    cp -rf ../output/html/* .
+    git config user.name "LibrePCB-Builder"
+    git config user.email "builder@librepcb.org"
+    git add -A
+    git commit -m "updated doxygen documentation"
+    git push origin gh-pages
+fi
+
