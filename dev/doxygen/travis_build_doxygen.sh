94566bbeadb848c74d113ab8ea24a01c253164af
ubruhin <urbibruhin@bluewin.ch>
2015-07-26 16:25:44 +0200

    updated travis-ci config

--- dev/doxygen/travis_build_doxygen.sh
+++ dev/doxygen/travis_build_doxygen.sh
@@ -3,7 +3,9 @@
 set -e # The -e flag causes the script to exit as soon as one command returns a non-zero exit code.
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then 
-    echo "Build doxygen documentation for branch ${TRAVIS_BRANCH}..."
+
+    BRANCH_NAME=$(echo ${TRAVIS_BRANCH} | sed -e 's/[^A-Za-z0-9._-]/_/g')
+    echo "Build doxygen documentation for branch $BRANCH_NAME..."
     
     echo "make clean:"
     make clean
@@ -16,16 +18,16 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     git clone -b gh-pages $DOXYGEN_REPOSITORY
     cd LibrePCB-Doxygen
     
-    echo "rm -rf ${TRAVIS_BRANCH}/*:"
-    mkdir -p ${TRAVIS_BRANCH}
-    rm -rf ${TRAVIS_BRANCH}/*
+    echo "rm -rf $BRANCH_NAME/*:"
+    mkdir -p $BRANCH_NAME
+    rm -rf $BRANCH_NAME/*
     
     echo "git add/commit/push:"
-    cp -rf ../output/html/* ./${TRAVIS_BRANCH}
+    cp -rf ../output/html/* ./$BRANCH_NAME
     git config user.name "LibrePCB-Builder"
     git config user.email "builder@librepcb.org"
-    git add -A
-    git commit -m "updated doxygen documentation of branch ${TRAVIS_BRANCH}"
+    git add -A > /dev/null
+    git commit -m "updated doxygen documentation of branch $BRANCH_NAME"
     git push origin gh-pages
 else
     echo "Pull request -> Do not build doxygen documentation"
