60aeeff49e5dfbc893b3ca0931ff30e13843b55d
ubruhin <urbibruhin@bluewin.ch>
2015-10-18 20:37:43 +0200

    added LibrePCB_git-master.conf and startup.sh

---
+++ dev/docker/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo 'Welcome to LibrePCB! Check out the following commands:'
+echo ' - git pull                                                      : update git working copy'
+echo ' - cd build && qmake -r ../librepcb.pro && make && make install  : compile and install LibrePCB'
+echo ' - qtcreator librepcb.pro                                        : open the project with Qt Creator'
+echo ' - librepcb                                                      : run LibrePCB'
+
