265120930b0bfa906dffde3f6f004ad7ebd16860
ubruhin <urbibruhin@bluewin.ch>
2016-02-04 23:31:35 +0100

    docker: splitted up "docker run" into "docker create" and "docker start"
    this makes changes in the container persistend between multiple sessions

--- dev/docker/build_container.sh
+++ dev/docker/build_container.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
 
+# build image "librepcb:git"
 docker build -t librepcb:git .
+
+# remove container "librepcb" if already existing
+STATUS=$(docker inspect --format="{{ .State.Status }}" librepcb 2> /dev/null)
+if [ $? -eq 0 ]; then
+  echo "WARNING: The container 'librepcb' will be removed! Continue? [Y/n]"
+  read answer
+  if [ "$answer" == "n" ]; then
+    echo "ABORTED"
+	  exit 1
+  fi
+  echo "WARNING: The container 'librepcb' is running! Kill it? [Y/n]"
+  read answer
+  if [ "$answer" == "n" ]; then
+    echo "ABORTED"
+	  exit 1
+  fi
+  docker rm -f librepcb
+fi
+
+# create new container "librepcb"
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+docker create -it --privileged \
+  -v $XSOCK:$XSOCK:rw \
+  -v $XAUTH:$XAUTH:rw \
+  --device=/dev/dri/card0:/dev/dri/card0 \
+  -e DISPLAY=$DISPLAY \
+  -e XAUTHORITY=$XAUTH \
+  --name librepcb \
+  librepcb:git
--- dev/docker/run_container.sh
+++ dev/docker/run_container.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-touch $XAUTH
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
-docker run \
-  -v $XSOCK:$XSOCK:rw \
-  -v $XAUTH:$XAUTH:rw \
-  --device=/dev/dri/card0:/dev/dri/card0 \
-  -e DISPLAY=$DISPLAY \
-  -e XAUTHORITY=$XAUTH \
-  -it librepcb:git $@
+if [[ ! -z "$@" ]]; then
+  docker start librepcb
+  docker exec -it librepcb $@
+  docker stop -t 0 librepcb
+else
+  docker start -i librepcb
+fi
