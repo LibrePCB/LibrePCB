cec5d08004ce47d726fb0b0bc2dc04f0aa0b799b
U. Bruhin <urbibruhin@bluewin.ch>
2016-07-11 13:04:21 +0200

    Cleanup main.cpp

--- librepcb/main.cpp
+++ librepcb/main.cpp
@@ -37,7 +37,183 @@ using namespace librepcb;
 using namespace librepcb::workspace;
 
 /*****************************************************************************************
- *  app.exec()
+ *  Function Prototypes
+ ****************************************************************************************/
+
+static void setApplicationMetadata() noexcept;
+static void installTranslations() noexcept;
+static void init3rdPartyLibs() noexcept;
+static void cleanup3rdPartyLibs() noexcept;
+static FilePath determineWorkspacePath() noexcept;
+static int openWorkspace(const FilePath& path) noexcept;
+static int appExec() noexcept;
+
+/*****************************************************************************************
+ *  main()
+ ****************************************************************************************/
+
+int main(int argc, char* argv[])
+{
+    Application app(argc, argv);
+
+    // --------------------------------- INITIALIZATION ----------------------------------
+
+    // Set the organization / application names must be done very early because some other
+    // classes will use these values (for example QSettings, Debug)!
+    setApplicationMetadata();
+
+    // Creates the Debug object which installs the message handler. This must be done as
+    // early as possible, but *after* setting application metadata (organization + name).
+    Debug::instance();
+
+    // Install translation files. This must be done before any widget is shown.
+    installTranslations();
+
+    // This is to remove the ugly frames around widgets in all status bars...
+    // (from http://www.qtcentre.org/threads/1904)
+    app.setStyleSheet("QStatusBar::item { border: 0px solid black; }");
+
+    // Initialize all 3rd party libraries
+    init3rdPartyLibs();
+
+    // --------------------------------- OPEN WORKSPACE ----------------------------------
+
+    // Get the path of the workspace to open (may show the first run wizard)
+    FilePath wsPath = determineWorkspacePath();
+
+    // Open the workspace and catch the return value
+    int retval = 0;
+    if (wsPath.isValid()) {
+        retval = openWorkspace(wsPath);
+    }
+
+    // -------------------------------- EXIT APPLICATION ---------------------------------
+
+    // Cleanup all 3rd party libraries
+    cleanup3rdPartyLibs();
+
+    return retval;
+}
+
+/*****************************************************************************************
+ *  setApplicationMetadata()
+ ****************************************************************************************/
+
+static void setApplicationMetadata() noexcept
+{
+    Application::setOrganizationName("LibrePCB");
+    Application::setOrganizationDomain("librepcb.org");
+#ifdef GIT_BRANCH
+    Application::setApplicationName(QString("LibrePCB_git-%1").arg(GIT_BRANCH));
+#else
+    Application::setApplicationName("LibrePCB");
+#endif
+    Application::setApplicationVersion(Version(QString("%1.%2.%3").arg(APP_VERSION_MAJOR)
+                                       .arg(APP_VERSION_MINOR).arg(APP_VERSION_PATCH)));
+}
+
+/*****************************************************************************************
+ *  installTranslations()
+ ****************************************************************************************/
+
+static void installTranslations() noexcept
+{
+    // Install Qt translations
+    QTranslator* qtTranslator = new QTranslator(qApp);
+    qtTranslator->load("qt_" % QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+    qApp->installTranslator(qtTranslator);
+
+    // Install system language translations (all system languages defined in the system settings, in the defined order)
+    QTranslator* systemTranslator = new QTranslator(qApp);
+    systemTranslator->load(QLocale::system(), "librepcb", "_", ":/i18n/");
+    qApp->installTranslator(systemTranslator);
+
+    // Install language translations (like "de" for German)
+    QTranslator* appTranslator1 = new QTranslator(qApp);
+    appTranslator1->load("librepcb_" % QLocale::system().name().split("_").at(0), ":/i18n");
+    qApp->installTranslator(appTranslator1);
+
+    // Install language/country translations (like "de_ch" for German/Switzerland)
+    QTranslator* appTranslator2 = new QTranslator(qApp);
+    appTranslator2->load("librepcb_" % QLocale::system().name(), ":/i18n");
+    qApp->installTranslator(appTranslator2);
+}
+
+/*****************************************************************************************
+ *  init3rdPartyLibs()
+ ****************************************************************************************/
+
+static void init3rdPartyLibs() noexcept
+{
+
+}
+
+/*****************************************************************************************
+ *  cleanup3rdPartyLibs()
+ ****************************************************************************************/
+
+static void cleanup3rdPartyLibs() noexcept
+{
+
+}
+
+/*****************************************************************************************
+ *  determineWorkspacePath()
+ ****************************************************************************************/
+
+static FilePath determineWorkspacePath() noexcept
+{
+    FilePath wsPath(Workspace::getMostRecentlyUsedWorkspacePath());
+    if (!Workspace::isValidWorkspacePath(wsPath)) {
+        FirstRunWizard wizard;
+        if (wizard.exec() == QDialog::Accepted) {
+            wsPath = wizard.getWorkspaceFilePath();
+            if (wizard.getCreateNewWorkspace()) {
+                if (!Workspace::createNewWorkspace(wsPath)) {
+                    QMessageBox::critical(0, Application::translate("Workspace",
+                        "Error"), Application::translate("Workspace", "Could not "
+                        "create the workspace directory. Check file permissions."));
+                    return FilePath(); // TODO: Show the wizard again instead of closing the application
+                }
+            }
+            Workspace::setMostRecentlyUsedWorkspacePath(wsPath);
+        } else {
+            return FilePath(); // abort
+        }
+    }
+    return wsPath;
+}
+
+/*****************************************************************************************
+ *  openWorkspace()
+ ****************************************************************************************/
+
+static int openWorkspace(const FilePath& path) noexcept
+{
+    try
+    {
+        Workspace ws(path);   // The Workspace constructor can throw an exception
+        ControlPanel p(ws);
+        p.show();
+
+        return appExec();
+    }
+    catch (UserCanceled& e)
+    {
+        return 0;
+    }
+    catch (Exception& e)
+    {
+        QMessageBox::critical(0, Application::translate("Workspace",
+            "Cannot open the workspace"), QString(Application::translate(
+            "Workspace", "The workspace \"%1\" cannot be opened: %2"))
+            .arg(path.toNative(), e.getUserMsg()));
+        return 0;
+    }
+}
+
+/*****************************************************************************************
+ *  appExec()
  ****************************************************************************************/
 
 static int appExec() noexcept
@@ -61,101 +237,4 @@ static int appExec() noexcept
     }
 
     return -1;
-}
-
-/*****************************************************************************************
- *  main()
- ****************************************************************************************/
-
-int main(int argc, char* argv[])
-{
-    Application app(argc, argv);
-
-    // Set the organization / application names must be done very early because some other
-    // classes will use these values (for example QSettings, Debug (for the file logging path))!
-    Application::setOrganizationName("LibrePCB");
-    Application::setOrganizationDomain("librepcb.org");
-#ifdef GIT_BRANCH
-    Application::setApplicationName(QString("LibrePCB_git-%1").arg(GIT_BRANCH));
-#else
-    Application::setApplicationName("LibrePCB");
-#endif
-    Application::setApplicationVersion(Version(QString("%1.%2.%3").arg(APP_VERSION_MAJOR).arg(APP_VERSION_MINOR).arg(APP_VERSION_PATCH)));
-
-
-    Debug::instance(); // this creates the Debug object and installs the message handler.
-
-
-    // Install Qt translations
-    QTranslator qtTranslator;
-    qtTranslator.load("qt_" % QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-    app.installTranslator(&qtTranslator);
-
-    // Install system language translations (all system languages defined in the system settings, in the defined order)
-    QTranslator systemTranslator;
-    systemTranslator.load(QLocale::system(), "librepcb", "_", ":/i18n/");
-    app.installTranslator(&systemTranslator);
-
-    // Install language translations (like "de" for German)
-    QTranslator appTranslator1;
-    appTranslator1.load("librepcb_" % QLocale::system().name().split("_").at(0), ":/i18n");
-    app.installTranslator(&appTranslator1);
-
-    // Install language/country translations (like "de_ch" for German/Switzerland)
-    QTranslator appTranslator2;
-    appTranslator2.load("librepcb_" % QLocale::system().name(), ":/i18n");
-    app.installTranslator(&appTranslator2);
-
-
-    Application::setQuitOnLastWindowClosed(false);
-
-
-    // this is to remove the ugly frames around widgets in all status bars...
-    // (from http://www.qtcentre.org/threads/1904)
-    app.setStyleSheet("QStatusBar::item { border: 0px solid black; }");
-
-
-
-    // Initialization finished, open the workspace (or show the first run wizard)...
-    FilePath wsPath(Workspace::getMostRecentlyUsedWorkspacePath());
-    if (!Workspace::isValidWorkspacePath(wsPath))
-    {
-        FirstRunWizard wizard;
-        if (wizard.exec() != QDialog::Accepted) return 0;
-        wsPath = wizard.getWorkspaceFilePath();
-        if (wizard.getCreateNewWorkspace())
-        {
-            if (!Workspace::createNewWorkspace(wsPath))
-            {
-                QMessageBox::critical(0, Application::translate("Workspace",
-                    "Error"), Application::translate("Workspace", "Could not "
-                    "create the workspace directory. Check file permissions."));
-                return 0; // TODO: Show the wizard again instead of closing the application
-            }
-        }
-        Workspace::setMostRecentlyUsedWorkspacePath(wsPath);
-    }
-
-    try
-    {
-        Workspace ws(wsPath);   // The Workspace constructor can throw an exception.
-        ControlPanel p(ws);
-        p.show();
-
-        return appExec();
-    }
-    catch (UserCanceled& e)
-    {
-        return 0; // quit the application
-    }
-    catch (Exception& e)
-    {
-        QMessageBox::critical(0, Application::translate("Workspace",
-            "Cannot open the workspace"), QString(Application::translate(
-            "Workspace", "The workspace \"%1\" cannot be opened: %2"))
-            .arg(wsPath.toNative(), e.getUserMsg()));
-        return 0; // quit the application
-    }
-
-    return 0;
 }
