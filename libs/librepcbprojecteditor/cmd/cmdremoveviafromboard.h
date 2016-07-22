5bec6a12ddbd9b9d95f6967e6bc757e1bf18cc43
U. Bruhin <urbibruhin@bluewin.ch>
2016-07-22 23:03:57 +0200

    Cleanup/Improve CmdRemoveSelectedBoardItems
    - Add high-level undo command CmdRemoveViaFromBoard
    - Add high-level undo command CmdRemoveDeviceFromBoard
    - Add high-level undo command CmdDetachBoardNetPointFromViaOrPad
    - Removing vias should now work properly in all cases (did not work when
      netlines were connected to them)

---
+++ libs/librepcbprojecteditor/cmd/cmdremoveviafromboard.h
@@ -0,0 +1,79 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * http://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBREPCB_PROJECT_CMDREMOVEVIAFROMBOARD_H
+#define LIBREPCB_PROJECT_CMDREMOVEVIAFROMBOARD_H
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+#include <QtCore>
+#include <librepcbcommon/undocommandgroup.h>
+
+/*****************************************************************************************
+ *  Namespace / Forward Declarations
+ ****************************************************************************************/
+namespace librepcb {
+namespace project {
+
+class BI_Via;
+
+/*****************************************************************************************
+ *  Class CmdRemoveViaFromBoard
+ ****************************************************************************************/
+
+/**
+ * @brief The CmdRemoveViaFromBoard class
+ */
+class CmdRemoveViaFromBoard final : public UndoCommandGroup
+{
+    public:
+
+        // Constructors / Destructor
+        CmdRemoveViaFromBoard() = delete;
+        CmdRemoveViaFromBoard(const CmdRemoveViaFromBoard& other) = delete;
+        CmdRemoveViaFromBoard(BI_Via& via) noexcept;
+        ~CmdRemoveViaFromBoard() noexcept;
+
+        // Operator Overloadings
+        CmdRemoveViaFromBoard& operator=(const CmdRemoveViaFromBoard& other) = delete;
+
+
+    private:
+
+        // Private Methods
+
+        /// @copydoc UndoCommand::performExecute()
+        bool performExecute() throw (Exception) override;
+
+
+        // Private Member Variables
+
+        // Attributes from the constructor
+        BI_Via& mVia;
+};
+
+/*****************************************************************************************
+ *  End of File
+ ****************************************************************************************/
+
+} // namespace project
+} // namespace librepcb
+
+#endif // LIBREPCB_PROJECT_CMDREMOVEVIAFROMBOARD_H
