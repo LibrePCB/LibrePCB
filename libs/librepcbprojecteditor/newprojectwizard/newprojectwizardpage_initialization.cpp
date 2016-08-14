1f4d2958f1597401e773123e11bd199d756185f7
U. Bruhin <urbibruhin@bluewin.ch>
2016-08-14 23:10:50 +0200

    ControlPanel: Add a wizard to create a new project
    This wizard makes it easier to create a new project, and it allows to
    specify some additional information how the project should be created.

---
+++ libs/librepcbprojecteditor/newprojectwizard/newprojectwizardpage_initialization.cpp
@@ -0,0 +1,146 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * http://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+#include <QtCore>
+#include <QtWidgets>
+#include "newprojectwizardpage_initialization.h"
+#include "ui_newprojectwizardpage_initialization.h"
+#include <librepcbcommon/fileio/filepath.h>
+
+/*****************************************************************************************
+ *  Namespace
+ ****************************************************************************************/
+namespace librepcb {
+namespace project {
+
+/*****************************************************************************************
+ *  Constructors / Destructor
+ ****************************************************************************************/
+
+NewProjectWizardPage_Initialization::NewProjectWizardPage_Initialization(QWidget *parent) noexcept :
+    QWizardPage(parent), mUi(new Ui::NewProjectWizardPage_Initialization)
+{
+    mUi->setupUi(this);
+    setPixmap(QWizard::LogoPixmap, QPixmap(":/img/actions/plus_2.png"));
+    setPixmap(QWizard::WatermarkPixmap, QPixmap(":/img/wizards/watermark.jpg"));
+
+    // signal/slot connections
+    connect(mUi->cbxAddSchematic, &QGroupBox::toggled,
+            this, &NewProjectWizardPage_Initialization::completeChanged);
+    connect(mUi->cbxAddBoard, &QGroupBox::toggled,
+            this, &NewProjectWizardPage_Initialization::completeChanged);
+    connect(mUi->edtSchematicName, &QLineEdit::textChanged,
+            this, &NewProjectWizardPage_Initialization::schematicNameChanged);
+    connect(mUi->edtBoardName, &QLineEdit::textChanged,
+            this, &NewProjectWizardPage_Initialization::boardNameChanged);
+
+    // insert values
+    mUi->edtSchematicName->setText("Main"); // do not translate this into other languages!
+    mUi->edtBoardName->setText("default");  // do not translate this into other languages!
+}
+
+NewProjectWizardPage_Initialization::~NewProjectWizardPage_Initialization() noexcept
+{
+}
+
+/*****************************************************************************************
+ *  Getters
+ ****************************************************************************************/
+
+bool NewProjectWizardPage_Initialization::getCreateSchematic() const noexcept
+{
+    return mUi->cbxAddSchematic->isChecked();
+}
+
+QString NewProjectWizardPage_Initialization::getSchematicName() const noexcept
+{
+    return mUi->edtSchematicName->text();
+}
+
+QString NewProjectWizardPage_Initialization::getSchematicFileName() const noexcept
+{
+    return mUi->lblSchematicFileName->text();
+}
+
+bool NewProjectWizardPage_Initialization::getCreateBoard() const noexcept
+{
+    return mUi->cbxAddBoard->isChecked();
+}
+
+QString NewProjectWizardPage_Initialization::getBoardName() const noexcept
+{
+    return mUi->edtBoardName->text();
+}
+
+QString NewProjectWizardPage_Initialization::getBoardFileName() const noexcept
+{
+    return mUi->lblBoardFileName->text();
+}
+
+/*****************************************************************************************
+ *  GUI Action Handlers
+ ****************************************************************************************/
+
+void NewProjectWizardPage_Initialization::schematicNameChanged(const QString& name) noexcept
+{
+    QString filename = FilePath::cleanFileName(name, FilePath::ReplaceSpaces | FilePath::ToLowerCase);
+    if (!filename.isEmpty()) filename.append(".xml");
+    mUi->lblSchematicFileName->setText(filename);
+    emit completeChanged();
+}
+
+void NewProjectWizardPage_Initialization::boardNameChanged(const QString& name) noexcept
+{
+    QString filename = FilePath::cleanFileName(name, FilePath::ReplaceSpaces | FilePath::ToLowerCase);
+    if (!filename.isEmpty()) filename.append(".xml");
+    mUi->lblBoardFileName->setText(filename);
+    emit completeChanged();
+}
+
+/*****************************************************************************************
+ *  Private Methods
+ ****************************************************************************************/
+
+bool NewProjectWizardPage_Initialization::isComplete() const noexcept
+{
+    // check base class
+    if (!QWizardPage::isComplete()) return false;
+
+    // check schematic filename
+    if (mUi->cbxAddSchematic->isChecked() && mUi->lblSchematicFileName->text().isEmpty()) {
+        return false;
+    }
+
+    // check board filename
+    if (mUi->cbxAddBoard->isChecked() && mUi->lblBoardFileName->text().isEmpty()) {
+        return false;
+    }
+
+    return true;
+}
+
+/*****************************************************************************************
+ *  End of File
+ ****************************************************************************************/
+
+} // namespace project
+} // namespace librepcb
---
+++ libs/librepcbprojecteditor/newprojectwizard/newprojectwizardpage_initialization.h
@@ -0,0 +1,97 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * http://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBREPCB_PROJECT_NEWPROJECTWIZARDPAGE_INITIALIZATION_H
+#define LIBREPCB_PROJECT_NEWPROJECTWIZARDPAGE_INITIALIZATION_H
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+#include <QtCore>
+#include <QtWidgets>
+
+/*****************************************************************************************
+ *  Namespace / Forward Declarations
+ ****************************************************************************************/
+
+namespace librepcb {
+namespace project {
+
+namespace Ui {
+class NewProjectWizardPage_Initialization;
+}
+
+/*****************************************************************************************
+ *  Class NewProjectWizardPage_Initialization
+ ****************************************************************************************/
+
+/**
+ * @brief The NewProjectWizardPage_Initialization class
+ *
+ * @author ubruhin
+ * @date 2016-08-13
+ */
+class NewProjectWizardPage_Initialization final : public QWizardPage
+{
+        Q_OBJECT
+
+    public:
+
+        // Constructors / Destructor
+
+        explicit NewProjectWizardPage_Initialization(QWidget* parent = nullptr) noexcept;
+        NewProjectWizardPage_Initialization(const NewProjectWizardPage_Initialization& other) = delete;
+        ~NewProjectWizardPage_Initialization() noexcept;
+
+        // Getters
+        bool getCreateSchematic() const noexcept;
+        QString getSchematicName() const noexcept;
+        QString getSchematicFileName() const noexcept;
+        bool getCreateBoard() const noexcept;
+        QString getBoardName() const noexcept;
+        QString getBoardFileName() const noexcept;
+
+        // Operator Overloadings
+        NewProjectWizardPage_Initialization& operator=(const NewProjectWizardPage_Initialization& rhs) = delete;
+
+
+    private: // GUI Action Handlers
+
+        void schematicNameChanged(const QString& name) noexcept;
+        void boardNameChanged(const QString& name) noexcept;
+
+
+    private: // Methods
+
+        bool isComplete() const noexcept override;
+
+
+    private: // Data
+
+        QScopedPointer<Ui::NewProjectWizardPage_Initialization> mUi;
+};
+
+/*****************************************************************************************
+ *  End of File
+ ****************************************************************************************/
+
+} // namespace project
+} // namespace librepcb
+
+#endif // LIBREPCB_PROJECT_NEWPROJECTWIZARDPAGE_INITIALIZATION_H
