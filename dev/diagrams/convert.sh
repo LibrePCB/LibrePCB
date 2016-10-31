c6a63c8c8f649f8577e02d7238e02a8e31e606fd
U. Bruhin <urbibruhin@bluewin.ch>
2016-10-31 10:27:06 +0100

    Fix missing spaces in SVG diagram texts

--- dev/diagrams/convert.sh
+++ dev/diagrams/convert.sh
@@ -3,8 +3,14 @@
 rm -r svg
 mkdir svg
 
+# convert all *.dia files to *.svg files
 find . -type f -name "*.dia" | while read f
 do
   mkdir -p "svg/"`dirname "$f"`
-  dia -e "svg/${f%.dia}.svg" "$f"
+  outfile="svg/${f%.dia}.svg"
+  dia -e "$outfile" "$f"
+  
+  # ugly workaround for missing spaces in SVG texts:
+  # http://stackoverflow.com/questions/27499032/svg-text-element-with-whitespace-not-preserved-in-ie#27499096
+  sed -i -e 's/<text font-size=/<text xml:space="preserve" font-size=/g' "$outfile"
 done
