843cdae249dd4c11abd6fe4595e75cf4867e5f1c
U. Bruhin <urbibruhin@bluewin.ch>
2017-02-04 19:07:38 +0100

    Add bash scripts for capturing screenshots

---
+++ dev/capture_screenshots.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Helper script to capture screenshots of LibrePCB windows.
+# Requirements: sudo apt-get install wmctrl shutter imagemagick
+# Note: Enable round corners in shutter preferences!
+
+set -euo pipefail
+
+function capture_screenshot {
+  echo "----- NEXT WINDOW: $1 -----"
+  sleep 5
+  wmctrl -a "$1" && wmctrl -r "$1" -e 0,100,100,1100,650
+  shutter -w="$1" -o "$2" -d 3 -c -e
+  convert -thumbnail 770x455 "$2" "thumbs/$2"
+}
+
+capture_screenshot "Control Panel" "control_panel.png"
+capture_screenshot "Board Editor" "board_editor.png"
+capture_screenshot "Schematic Editor" "schematic_editor.png"
+capture_screenshot "Add Component" "add_component_dialog.png"
+capture_screenshot "Library Manager" "library_manager.png"
+#capture_screenshot "Library Editor" "library_editor.png"
+
