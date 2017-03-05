897eabdd6ff35b736d109b9fcee95b3c7467097b
U. Bruhin <urbibruhin@bluewin.ch>
2017-03-05 14:07:43 +0100

    Use safe shell settings in build script for Docker

--- dev/docker/make_librepcb.sh
+++ dev/docker/make_librepcb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# set shell settings (see https://sipb.mit.edu/doc/safe-shell/)
+set -eufv -o pipefail
+
 mkdir -p /opt/build-librepcb-Desktop-Debug
 pushd /opt/build-librepcb-Desktop-Debug
 qmake -r ../LibrePCB/librepcb.pro
