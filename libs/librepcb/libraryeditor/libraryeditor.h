e37bbacd3e629d063c9a961f7a672ceadb59e399
U. Bruhin <urbibruhin@bluewin.ch>
2017-08-19 00:09:09 +0200

    LibraryEditor: Add element editor widgets

--- libs/librepcb/libraryeditor/libraryeditor.h
+++ libs/librepcb/libraryeditor/libraryeditor.h
@@ -23,21 +23,34 @@
 /*****************************************************************************************
  *  Includes
  ****************************************************************************************/
+#include <memory>
 #include <QtCore>
 #include <QtWidgets>
 #include <librepcb/common/exceptions.h>
+#include <librepcb/common/graphics/graphicslayer.h>
+#include <librepcb/common/fileio/directorylock.h>
+#include <librepcb/common/units/all_length_units.h>
 
 /*****************************************************************************************
  *  Namespace / Forward Declarations
  ****************************************************************************************/
 namespace librepcb {
 
+class UndoStackActionGroup;
+class ExclusiveActionGroup;
+
 namespace workspace {
 class Workspace;
 }
 
 namespace library {
+
+class Library;
+
 namespace editor {
+
+class EditorWidgetBase;
+class LibraryOverviewWidget;
 
 namespace Ui {
 class LibraryEditor;
@@ -50,28 +63,110 @@ class LibraryEditor;
 /**
  * @brief The LibraryEditor class
  *
- * @todo this is only a stub class...
+ * @author ubruhin
+ * @date 2015-06-28
  */
-class LibraryEditor final : public QMainWindow
+class LibraryEditor final : public QMainWindow, public IF_GraphicsLayerProvider
 {
         Q_OBJECT
 
     public:
 
         // Constructors / Destructor
-        explicit LibraryEditor(workspace::Workspace& workspace);
+        LibraryEditor() = delete;
+        LibraryEditor(const LibraryEditor& other) = delete;
+        LibraryEditor(workspace::Workspace& ws, QSharedPointer<Library> lib);
         ~LibraryEditor() noexcept;
 
+        /**
+         * @copydoc librepcb::IF_GraphicsLayerProvider::getLayer()
+         */
+        GraphicsLayer* getLayer(const QString& name) const noexcept override {
+            foreach (GraphicsLayer* layer, mLayers) {
+                if (layer->getName() == name) {
+                    return layer;
+                }
+            }
+            return nullptr;
+        }
 
-    private:
+        /**
+         * @copydoc librepcb::IF_GraphicsLayerProvider::getAllLayers()
+         */
+        QList<GraphicsLayer*> getAllLayers() const noexcept override {
+            return mLayers;
+        }
 
-        // make some methods inaccessible...
-        LibraryEditor(const LibraryEditor& other);
-        LibraryEditor& operator=(const LibraryEditor& rhs);
+        /**
+         * @brief Close the library editor (this will destroy this object!)
+         *
+         * If there are unsaved changes to the library, this method will ask the user
+         * whether the changes should be saved or not. If the user clicks on "cancel"
+         * or the library could not be saved successfully, this method will return false.
+         * If there was no such error, this method will call QObject#deleteLater() which
+         * means that this object will be deleted in the Qt's event loop.
+         *
+         * @warning This method can be called both from within this class and from outside
+         *          this class (for example from the #ControlPanel). But if you call this
+         *          method from outside this class, you may have to delete the object
+         *          yourself afterwards! In special cases, the deleteLater() mechanism
+         *          could lead in fatal errors otherwise!
+         *
+         * @param askForSave    If true and there are unsaved changes, this method shows
+         *                      a message box to ask whether the library should be saved
+         *                      or not. If false, the library will NOT be saved.
+         *
+         * @return true on success (editor closed), false on failure (editor stays open)
+         */
+        bool closeAndDestroy(bool askForSave) noexcept;
 
-        // Attributes
+
+        // Operator Overloadings
+        LibraryEditor& operator=(const LibraryEditor& rhs) = delete;
+
+
+    private: // GUI Event Handlers
+        void newElementTriggered() noexcept;
+        void saveTriggered() noexcept;
+        void removeElementTriggered() noexcept;
+        void rotateCwTriggered() noexcept;
+        void rotateCcwTriggered() noexcept;
+        void removeTriggered() noexcept;
+        void abortCommandTriggered() noexcept;
+        void zoomInTriggered() noexcept;
+        void zoomOutTriggered() noexcept;
+        void zoomAllTriggered() noexcept;
+        void editGridPropertiesTriggered() noexcept;
+        void editComponentCategoryTriggered(const FilePath& fp) noexcept;
+        void editPackageCategoryTriggered(const FilePath& fp) noexcept;
+        void editSymbolTriggered(const FilePath& fp) noexcept;
+        void editPackageTriggered(const FilePath& fp) noexcept;
+        void editComponentTriggered(const FilePath& fp) noexcept;
+        void editDeviceTriggered(const FilePath& fp) noexcept;
+        template <typename ElementType, typename EditWidgetType>
+        void editLibraryElementTriggered(const FilePath& fp, bool isNewElement) noexcept;
+        void currentTabChanged(int index) noexcept;
+        bool tabCloseRequested(int index) noexcept;
+        void cursorPositionChanged(const Point& pos) noexcept;
+
+
+    private: // Methods
+        void setActiveEditorWidget(EditorWidgetBase* widget);
+        void updateTabTitles() noexcept;
+        void closeEvent(QCloseEvent* event) noexcept override;
+        void addLayer(const QString& name) noexcept;
+
+
+    private: // Data
+
         workspace::Workspace& mWorkspace;
-        Ui::LibraryEditor* mUi;
+        QSharedPointer<Library> mLibrary;
+        QScopedPointer<Ui::LibraryEditor> mUi;
+        QScopedPointer<UndoStackActionGroup> mUndoStackActionGroup;
+        QScopedPointer<ExclusiveActionGroup> mToolsActionGroup;
+        QList<GraphicsLayer*> mLayers;
+        EditorWidgetBase* mCurrentEditorWidget;
+        DirectoryLock mLock;
 };
 
 /*****************************************************************************************
