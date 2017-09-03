2fcc204439764f23b49c5b58318ed13a87910f0b
U. Bruhin <urbibruhin@bluewin.ch>
2017-09-03 20:23:22 +0200

    Add new common class StatusBar

---
+++ libs/librepcb/common/widgets/statusbar.cpp
@@ -0,0 +1,121 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * http://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+#include <QtCore>
+#include <QtWidgets>
+#include "statusbar.h"
+
+/*****************************************************************************************
+ *  Namespace
+ ****************************************************************************************/
+namespace librepcb {
+
+/*****************************************************************************************
+ *  Constructors / Destructor
+ ****************************************************************************************/
+
+StatusBar::StatusBar(QWidget* parent) noexcept :
+    QStatusBar(parent), mFields(0)
+{
+    // absolute position x
+    mAbsPosXLabel.reset(new QLabel());
+    mAbsPosXLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    mAbsPosXLabel->setFont(QFont("monospace"));
+    addPermanentWidget(mAbsPosXLabel.data());
+
+    // absolute position y
+    mAbsPosYLabel.reset(new QLabel());
+    mAbsPosYLabel->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    mAbsPosYLabel->setFont(QFont("monospace"));
+    addPermanentWidget(mAbsPosYLabel.data());
+
+    // progress bar
+    mProgressBar.reset(new QProgressBar());
+    mProgressBar->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    mProgressBar->setFixedWidth(200);
+    mProgressBar->setMinimum(0);
+    mProgressBar->setMaximum(100);
+    addPermanentWidget(mProgressBar.data());
+
+    // progress bar placeholder to reserve space
+    mProgressBarPlaceHolder.reset(new QWidget());
+    mProgressBarPlaceHolder->setFixedWidth(200);
+    addPermanentWidget(mProgressBarPlaceHolder.data());
+
+    // init
+    setFields(0);
+    setAbsoluteCursorPosition(Point());
+    setProgressBarVisible(false);
+    setProgressBarPercent(0);
+}
+
+StatusBar::~StatusBar() noexcept
+{
+}
+
+/*****************************************************************************************
+ *  Setters
+ ****************************************************************************************/
+
+void StatusBar::setFields(Fields fields) noexcept
+{
+    mAbsPosXLabel->setVisible(fields & AbsolutePosition);
+    mAbsPosYLabel->setVisible(fields & AbsolutePosition);
+}
+
+void StatusBar::setField(Field field, bool enable) noexcept
+{
+    if (enable) {
+        mFields |= field;
+    } else {
+        mFields &= ~field;
+    }
+    setFields(mFields);
+}
+
+void StatusBar::setAbsoluteCursorPosition(const Point& pos) noexcept
+{
+    mAbsPosXLabel->setText(QString("X:%1mm").arg(pos.getX().toMm(), 12, 'f', 6));
+    mAbsPosYLabel->setText(QString("Y:%1mm").arg(pos.getY().toMm(), 12, 'f', 6));
+}
+
+void StatusBar::setProgressBarVisible(bool visible) noexcept
+{
+    mProgressBar->setVisible(visible);
+    mProgressBarPlaceHolder->setVisible(!visible);
+}
+
+void StatusBar::setProgressBarTextFormat(const QString& format) noexcept
+{
+    mProgressBar->setFormat(format);
+}
+
+void StatusBar::setProgressBarPercent(int percent) noexcept
+{
+    mProgressBar->setValue(percent);
+}
+
+/*****************************************************************************************
+ *  End of File
+ ****************************************************************************************/
+
+} // namespace librepcb
---
+++ libs/librepcb/common/widgets/statusbar.h
@@ -0,0 +1,97 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * http://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBREPCB_STATUSBAR_H
+#define LIBREPCB_STATUSBAR_H
+
+/*****************************************************************************************
+ *  Includes
+ ****************************************************************************************/
+#include <QtCore>
+#include <QtWidgets>
+#include "../units/point.h"
+
+/*****************************************************************************************
+ *  Namespace / Forward Declarations
+ ****************************************************************************************/
+namespace librepcb {
+
+/*****************************************************************************************
+ *  Class StatusBar
+ ****************************************************************************************/
+
+/**
+ * @brief The StatusBar class extends QStatusBar for some commonly used fields
+ *
+ * @author ubruhin
+ * @date 2017-09-03
+ */
+class StatusBar final : public QStatusBar
+{
+        Q_OBJECT
+
+    public:
+        // Types
+        enum Field {
+            AbsolutePosition    = 1<<0,
+            ProgressBar         = 1<<1,
+        };
+        Q_DECLARE_FLAGS(Fields, Field);
+
+        // Constructors / Destructor
+        explicit StatusBar(QWidget* parent = nullptr) noexcept;
+        StatusBar(const StatusBar& other) = delete;
+        ~StatusBar() noexcept;
+
+        // Getters
+        Fields getFields() const noexcept {return mFields;}
+
+        // Setters
+        void setFields(Fields fields) noexcept;
+        void setField(Field field, bool enable) noexcept;
+        void setAbsoluteCursorPosition(const Point& pos) noexcept;
+        void setProgressBarVisible(bool visible) noexcept;
+        void setProgressBarTextFormat(const QString& format) noexcept;
+        void setProgressBarPercent(int percent) noexcept;
+
+        // General Methods
+        void showProgressBar() noexcept {setProgressBarVisible(true);}
+        void hideProgressBar() noexcept {setProgressBarVisible(false);}
+
+        // Operator Overloadings
+        StatusBar& operator=(const StatusBar& rhs) = delete;
+
+
+    private: // Data
+        Fields mFields;
+        QScopedPointer<QLabel> mAbsPosXLabel;
+        QScopedPointer<QLabel> mAbsPosYLabel;
+        QScopedPointer<QProgressBar> mProgressBar;
+        QScopedPointer<QWidget> mProgressBarPlaceHolder;
+};
+
+/*****************************************************************************************
+ *  End of File
+ ****************************************************************************************/
+
+} // namespace librepcb
+
+Q_DECLARE_OPERATORS_FOR_FLAGS(librepcb::StatusBar::Fields)
+
+#endif // LIBREPCB_STATUSBAR_H
