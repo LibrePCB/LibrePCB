983502c153d848e26e3317028fe8d1da9727959e
Eduardo Sánchez Muñoz <esm@eduardosm.net>
2018-11-06 19:36:44 +0100

    Length: Add more tests. Some test data cases are commented because of limits of 'testing::Values'.

--- tests/unittests/common/lengthtest.cpp
+++ tests/unittests/common/lengthtest.cpp
@@ -39,6 +39,8 @@ typedef struct {
   QString orig_str;
   Length  value;
   QString gen_str;
+  Length grid_interval;
+  Length mapped_to_grid;
 } LengthTestData;
 
 /*******************************************************************************
@@ -68,24 +70,12 @@ TEST_P(LengthTest, testToMmString) {
   }
 }
 
-TEST(LengthTest, testSnapToGrid) {
-  EXPECT_EQ(Length(0).mappedToGrid(10), Length(0));
-  EXPECT_EQ(Length(10).mappedToGrid(0), Length(10));
-  EXPECT_EQ(Length(-10).mappedToGrid(0), Length(-10));
-  EXPECT_EQ(Length(10).mappedToGrid(1), Length(10));
-  EXPECT_EQ(Length(-10).mappedToGrid(1), Length(-10));
-  EXPECT_EQ(Length(8).mappedToGrid(10), Length(10));
-  EXPECT_EQ(Length(2).mappedToGrid(10), Length(0));
-  EXPECT_EQ(Length(-8).mappedToGrid(10), Length(-10));
-  EXPECT_EQ(Length(-2).mappedToGrid(10), Length(0));
-  EXPECT_EQ(Length(18).mappedToGrid(10), Length(20));
-  EXPECT_EQ(Length(12).mappedToGrid(10), Length(10));
-  EXPECT_EQ(Length(-18).mappedToGrid(10), Length(-20));
-  EXPECT_EQ(Length(-12).mappedToGrid(10), Length(-10));
-  EXPECT_EQ(Length(10).mappedToGrid(10), Length(10));
-  EXPECT_EQ(Length(-10).mappedToGrid(10), Length(-10));
-  EXPECT_EQ(Length(20).mappedToGrid(20), Length(20));
-  EXPECT_EQ(Length(-20).mappedToGrid(20), Length(-20));
+TEST_P(LengthTest, testSnapToGrid) {
+  const LengthTestData& data = GetParam();
+
+  if (data.valid) {
+    EXPECT_EQ(data.value.mappedToGrid(data.grid_interval), data.mapped_to_grid);
+  }
 }
 
 /*******************************************************************************
@@ -94,44 +84,64 @@ TEST(LengthTest, testSnapToGrid) {
 
 // clang-format off
 INSTANTIATE_TEST_CASE_P(LengthTest, LengthTest, ::testing::Values(
-    LengthTestData({true,  "0",              Length(0),           "0.0"         }),
-    LengthTestData({true,  "1",              Length(1000000),     "1.0"         }),
-    LengthTestData({true,  "-1",             Length(-1000000),    "-1.0"        }),
-    LengthTestData({true,  "0.000001",       Length(1),           "0.000001"    }),
-    LengthTestData({true,  "-0.000001",      Length(-1),          "-0.000001"   }),
-    LengthTestData({true,  "1e-6",           Length(1),           "0.000001"    }),
-    LengthTestData({true,  "-1e-6",          Length(-1),          "-0.000001"   }),
-    LengthTestData({true,  "1.000001",       Length(1000001),     "1.000001"    }),
-    LengthTestData({true,  "-1.000001",      Length(-1000001),    "-1.000001"   }),
-    LengthTestData({true,  "1e3",            Length(1000000000),  "1000.0"      }),
-    LengthTestData({true,  "-1e3",           Length(-1000000000), "-1000.0"     }),
-    LengthTestData({true,  ".1",             Length(100000),      "0.1"         }),
-    LengthTestData({true,  "1.",             Length(1000000),     "1.0"         }),
-    LengthTestData({true,  "2147483647e-6",  Length(2147483647),  "2147.483647" }),
-    LengthTestData({true,  "-2147483648e-6", Length(-2147483648), "-2147.483648"}),
-    LengthTestData({true,  "9",              Length(9000000),     "9.0"         }),
-    LengthTestData({true,  "9.9",            Length(9900000),     "9.9"         }),
-    LengthTestData({true,  "0.9",            Length(900000),      "0.9"         }),
-    LengthTestData({true,  "0.99",           Length(990000),      "0.99"        }),
-    LengthTestData({true,  "0.09",           Length(90000),       "0.09"        }),
-    LengthTestData({true,  "0.099",          Length(99000),       "0.099"       }),
-    LengthTestData({true,  "0.009",          Length(9000),        "0.009"       }),
-    LengthTestData({true,  "0.0099",         Length(9900),        "0.0099"      }),
-    LengthTestData({true,  "0.0009",         Length(900),         "0.0009"      }),
-    LengthTestData({true,  "0.00099",        Length(990),         "0.00099"     }),
-    LengthTestData({true,  "0.00009",        Length(90),          "0.00009"     }),
-    LengthTestData({true,  "0.000099",       Length(99),          "0.000099"    }),
-    LengthTestData({true,  "0.000009",       Length(9),           "0.000009"    }),
+    // from/to mm
+    LengthTestData({true,  "0",              Length(0),           "0.0",          Length(0),  Length(0)           }),
+    LengthTestData({true,  "1",              Length(1000000),     "1.0",          Length(0),  Length(1000000)     }),
+    LengthTestData({true,  "-1",             Length(-1000000),    "-1.0",         Length(0),  Length(-1000000)    }),
+    LengthTestData({true,  "0.000001",       Length(1),           "0.000001",     Length(0),  Length(1)           }),
+    LengthTestData({true,  "-0.000001",      Length(-1),          "-0.000001",    Length(0),  Length(-1)          }),
+    LengthTestData({true,  "1e-6",           Length(1),           "0.000001",     Length(0),  Length(1)           }),
+    LengthTestData({true,  "-1e-6",          Length(-1),          "-0.000001",    Length(0),  Length(-1)          }),
+    LengthTestData({true,  "1.000001",       Length(1000001),     "1.000001",     Length(0),  Length(1000001)     }),
+    LengthTestData({true,  "-1.000001",      Length(-1000001),    "-1.000001",    Length(0),  Length(-1000001)    }),
+    LengthTestData({true,  "1e3",            Length(1000000000),  "1000.0",       Length(0),  Length(1000000000)  }),
+    LengthTestData({true,  "-1e3",           Length(-1000000000), "-1000.0",      Length(0),  Length(-1000000000) }),
+    LengthTestData({true,  ".1",             Length(100000),      "0.1",          Length(0),  Length(100000)      }),
+    LengthTestData({true,  "1.",             Length(1000000),     "1.0",          Length(0),  Length(1000000)     }),
+    LengthTestData({true,  "2147483647e-6",  Length(2147483647),  "2147.483647",  Length(0),  Length(2147483647)  }),
+    LengthTestData({true,  "-2147483648e-6", Length(-2147483648), "-2147.483648", Length(0),  Length(-2147483648) }),
+    LengthTestData({true,  "9",              Length(9000000),     "9.0",          Length(0),  Length(9000000)     }),
+    LengthTestData({true,  "9.9",            Length(9900000),     "9.9",          Length(0),  Length(9900000)     }),
+    LengthTestData({true,  "0.9",            Length(900000),      "0.9",          Length(0),  Length(900000)      }),
+    LengthTestData({true,  "0.99",           Length(990000),      "0.99",         Length(0),  Length(990000)      }),
+    LengthTestData({true,  "0.09",           Length(90000),       "0.09",         Length(0),  Length(90000)       }),
+    LengthTestData({true,  "0.099",          Length(99000),       "0.099",        Length(0),  Length(99000)       }),
+    LengthTestData({true,  "0.009",          Length(9000),        "0.009",        Length(0),  Length(9000)        }),
+    LengthTestData({true,  "0.0099",         Length(9900),        "0.0099",       Length(0),  Length(9900)        }),
+    LengthTestData({true,  "0.0009",         Length(900),         "0.0009",       Length(0),  Length(900)         }),
+    LengthTestData({true,  "0.00099",        Length(990),         "0.00099",      Length(0),  Length(990)         }),
+    LengthTestData({true,  "0.00009",        Length(90),          "0.00009",      Length(0),  Length(90)          }),
+    LengthTestData({true,  "0.000099",       Length(99),          "0.000099",     Length(0),  Length(99)          }),
+    LengthTestData({true,  "0.000009",       Length(9),           "0.000009",     Length(0),  Length(9)           }),
+
+    // snap to grid
+    LengthTestData({true,  "0.0",            Length(0),           "0.0",          Length(10), Length(0)           }),
+    LengthTestData({true,  "0.00001",        Length(10),          "0.00001",      Length(0),  Length(10)          }),
+    LengthTestData({true,  "-0.00001",       Length(-10),         "-0.00001",     Length(0),  Length(-10)         }),
+    LengthTestData({true,  "0.00001",        Length(10),          "0.00001",      Length(1),  Length(10)          }),
+    LengthTestData({true,  "-0.00001",       Length(-10),         "-0.00001",     Length(1),  Length(-10)         }),
+    LengthTestData({true,  "0.000008",       Length(8),           "0.000008",     Length(10), Length(10)          }),
+    LengthTestData({true,  "0.000002",       Length(2),           "0.000002",     Length(10), Length(0)           }),
+    LengthTestData({true,  "-0.000008",      Length(-8),          "-0.000008",    Length(10), Length(-10)         }),
+    LengthTestData({true,  "-0.000002",      Length(-2),          "-0.000002",    Length(10), Length(0)           }),
+    LengthTestData({true,  "0.000018",       Length(18),          "0.000018",     Length(10), Length(20)          }),
+    LengthTestData({true,  "0.000012",       Length(12),          "0.000012",     Length(10), Length(10)          }),
+    LengthTestData({true,  "-0.000018",      Length(-18),         "-0.000018",    Length(10), Length(-20)         }),
+    LengthTestData({true,  "-0.000012",      Length(-12),         "-0.000012",    Length(10), Length(-10)         }),
+    LengthTestData({true,  "0.00001",        Length(10),          "0.00001",      Length(10), Length(10)          }),
+    //LengthTestData({true,  "-0.00001",       Length(-10),         "-0.00001",     Length(10), Length(-10)         }),
+    //LengthTestData({true,  "0.00002",        Length(20),          "0.00002",      Length(10), Length(20)          }),
+    //LengthTestData({true,  "-0.00002",       Length(-20),         "-0.00002",     Length(10), Length(-20)         }),
 
     // invalid cases
-    LengthTestData({false, "",               Length(),            QString()     }),
-    LengthTestData({false, ".",              Length(),            QString()     }),
-    LengthTestData({false, "0e",             Length(),            QString()     }),
-    LengthTestData({false, "0e+",            Length(),            QString()     }),
-    LengthTestData({false, "0e-",            Length(),            QString()     }),
-    LengthTestData({false, "0.0000001",      Length(),            QString()     }),
-    LengthTestData({false, "1e-7",           Length(),            QString()     }),
-    LengthTestData({false, "1e1000",         Length(),            QString()     })
+    LengthTestData({false, "",               Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, ".",              Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, "0e",             Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, "0e+",            Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, "0e-",            Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, "0.0000001",      Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, "1e-7",           Length(),            QString(),      Length(),   Length()            }),
+    LengthTestData({false, "1e1000",         Length(),            QString(),      Length(),   Length()            })
 ));
 // clang-format on
 
