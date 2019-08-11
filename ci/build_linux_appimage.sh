83425f668aa46ca257f9d61e531359f6160078ef
U. Bruhin <urbibruhin@bluewin.ch>
2019-08-11 22:59:57 +0200

    CI: Fix AppImage build error

--- ci/build_linux_appimage.sh
+++ ci/build_linux_appimage.sh
@@ -3,10 +3,15 @@
 # set shell settings (see https://sipb.mit.edu/doc/safe-shell/)
 set -euv -o pipefail
 
+# Manually specify icon to be used for the AppImage
+cp "./build/install/opt/share/icons/hicolor/scalable/apps/org.librepcb.LibrePCB.svg" "./build/install/org.librepcb.LibrePCB.svg"
+
+# Build CLI AppImage
 cp -r "./build/install" "./build/install-cli"
 mv -f "./build/install-cli/opt/bin/librepcb-cli" "./build/install-cli/opt/bin/librepcb"
 linuxdeployqt "./build/install-cli/opt/share/applications/org.librepcb.LibrePCB.desktop" -bundle-non-qt-libs -appimage
-mv ./LibrePCB-x86_64.AppImage ./LibrePCB-CLI-x86_64.AppImage
+mv ./LibrePCB-*-x86_64.AppImage ./artifacts/nightly_builds/librepcb-cli-nightly-linux-x86_64.AppImage
+
+# Build LibrePCB AppImage
 linuxdeployqt "./build/install/opt/share/applications/org.librepcb.LibrePCB.desktop" -bundle-non-qt-libs -appimage
-cp ./LibrePCB-x86_64.AppImage ./artifacts/nightly_builds/librepcb-nightly-linux-x86_64.AppImage
-cp ./LibrePCB-CLI-x86_64.AppImage ./artifacts/nightly_builds/librepcb-cli-nightly-linux-x86_64.AppImage
+cp ./LibrePCB-*-x86_64.AppImage ./artifacts/nightly_builds/librepcb-nightly-linux-x86_64.AppImage
