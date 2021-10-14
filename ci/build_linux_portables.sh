f9d8f2f7e764670e460ed20c5400ee275d8df093
U. Bruhin <urbibruhin@bluewin.ch>
2021-10-14 22:51:48 +0200

    CI: Refactor and clean up helper scripts
    - Simplify install_dependencies.sh by sharing the same "pip install"
      statements for all operating systems
    - Remove legacy MacOS workarounds from install_dependencies.sh
    - Merge build_linux_archive.sh and build_linux_appimage.sh into a new
      file build_linux_portables.sh since they do very similar tasks and
      can share some logic

--- ci/build_linux_portables.sh
+++ ci/build_linux_portables.sh
@@ -3,7 +3,7 @@
 # set shell settings (see https://sipb.mit.edu/doc/safe-shell/)
 set -euv -o pipefail
 
-# Build CLI AppImage
+# Build CLI AppImage.
 cp -r "./build/install" "./build/appimage-cli"
 mv -f "./build/appimage-cli/opt/bin/librepcb-cli" "./build/appimage-cli/opt/bin/librepcb"
 cp "./build/appimage-cli/opt/share/icons/hicolor/scalable/apps/org.librepcb.LibrePCB.svg" \
@@ -11,8 +11,15 @@ cp "./build/appimage-cli/opt/share/icons/hicolor/scalable/apps/org.librepcb.Libr
 linuxdeployqt "./build/appimage-cli/opt/share/applications/org.librepcb.LibrePCB.desktop" -bundle-non-qt-libs -appimage
 mv ./LibrePCB-*-x86_64.AppImage ./artifacts/nightly_builds/librepcb-cli-nightly-linux-x86_64.AppImage
 
-# Build LibrePCB AppImage
+# Build LibrePCB AppImage.
 cp -r "./build/install" "./build/appimage"
 cp "./build/appimage/opt/share/icons/hicolor/scalable/apps/org.librepcb.LibrePCB.svg" "./build/appimage/org.librepcb.LibrePCB.svg"
 linuxdeployqt "./build/appimage/opt/share/applications/org.librepcb.LibrePCB.desktop" -bundle-non-qt-libs -appimage
 cp ./LibrePCB-*-x86_64.AppImage ./artifacts/nightly_builds/librepcb-nightly-linux-x86_64.AppImage
+
+# Run linuxdeployqt to bundle all libraries into the portable packages.
+linuxdeployqt "./build/install/opt/bin/librepcb-cli" -bundle-non-qt-libs -always-overwrite
+linuxdeployqt "./build/install/opt/bin/librepcb" -bundle-non-qt-libs -always-overwrite
+
+# Copy to artifacts.
+cp -r "./build/install/opt" "./artifacts/nightly_builds/librepcb-nightly-linux-x86_64"
--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -3,61 +3,49 @@
 # set shell settings (see https://sipb.mit.edu/doc/safe-shell/)
 set -euv -o pipefail
 
-QTIFW_VERSION="3.2.2"
-QTIFW_URL_BASE="https://download.qt.io/official_releases/qt-installer-framework/$QTIFW_VERSION"
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# Install dependencies on Linux
-if [ "$OS" = "linux" ]
+# On MacOS, we have to install some dependencies since we can't use a custom
+# Docker container with all tools preinstalled.
+if [ "$OS" = "mac" ]
 then
-
-  # python packages
-  pip install --user -r "$DIR/../tests/cli/requirements.txt"
-  pip install --user -r "$DIR/../tests/funq/requirements.txt"
-  export PATH="$PATH:`python -m site --user-base`/bin"
-
-# Install dependencies on OS X
-elif [ "$OS" = "mac" ]
-then
-
-  # MacOS resp. homebrew needs workarounds to make it less unstable.
-  # See https://github.com/actions/virtual-environments/issues/1811
-  brew uninstall openssl@1.0.2t || true
-  brew uninstall python@2.7.17 || true
-  brew untap local/openssl || true
-  brew untap local/python2 || true
-
   # Update homebrow to avoid issues due to outdated package database. But
   # because even the update sometimes fails, let's ignore any errors with
   # "|| true" (Apple-style error handling). Maybe this way we get succussful
   # builds even if homebrow failed, which saves a lot of time and nerves.
   brew update || true
 
+  # Make python3/pip3 the default
+  export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+
   # Install Qt
   brew install qt5
   brew link --force qt5
 
-  # Install Python packages
-  pip3 install --user -r ./tests/cli/requirements.txt
-  pip3 install --user -r ./tests/funq/requirements.txt
-  export PATH="$PATH:`python3 -m site --user-base`/bin"
-
-  # Qt Installer Framework
+  # Install Qt Installer Framework
+  QTIFW_VERSION="3.2.2"
+  QTIFW_URL_BASE="https://download.qt.io/official_releases/qt-installer-framework/$QTIFW_VERSION"
   wget -cq "$QTIFW_URL_BASE/QtInstallerFramework-mac-x64.dmg"
   hdiutil attach ./QtInstallerFramework-mac-x64.dmg
   QTIFW_PATH="/Volumes/QtInstallerFramework-mac-x64/QtInstallerFramework-mac-x64.app/Contents/MacOS/QtInstallerFramework-mac-x64"
   chmod +x $QTIFW_PATH
   $QTIFW_PATH --script ./ci/qtifw-installer-noninteractive.qs --no-force-installations -v
   export PATH="/Users/$USER/Qt/QtIFW-$QTIFW_VERSION/bin:$PATH"
-
-# Install dependencies on Windows (inside MSYS2)
-elif [ "$OS" = "windows" ]
-then
-
-  # python packages
-  export FUNQ_MAKE_PATH="mingw32-make"
-  pip install -r ./tests/cli/requirements.txt
-  pip install -r ./tests/funq/requirements.txt
-
 fi
+
+# Configure pip
+if [ "$OS" != "windows" ]
+then
+  # We're not root, thus no permissions to install packages globally
+  PIP_USER_INSTALL="--user"
+  export PATH="$PATH:`python -m site --user-base`/bin"
+else
+  # Extending PATH does not work, thus installing Python packages globally
+  PIP_USER_INSTALL=""
+  # Set custom "make" command for building Funq
+  export FUNQ_MAKE_PATH="mingw32-make"
+fi
+
+# Install Python packages
+pip install $PIP_USER_INSTALL -r "$DIR/../tests/cli/requirements.txt"
+pip install $PIP_USER_INSTALL -r "$DIR/../tests/funq/requirements.txt"
