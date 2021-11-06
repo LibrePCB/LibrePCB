e086c1524be599b1e96fdfaceb33efaeea56d810
Luigi Baldoni <aloisio@gmx.com>
2021-11-06 15:27:44 +0100

    Dxflib doesn't supply .pc file, fix cmake script

--- cmake/FindDxflib.cmake
+++ cmake/FindDxflib.cmake
@@ -15,15 +15,19 @@ if(EXISTS "${DXFLIB_SUBMODULE_BASEPATH}"
   return()
 endif()
 
-# Otherwise, try to find shared library on the system via pkg-config
-find_package(PkgConfig QUIET)
-if(PKGCONFIG_FOUND)
-  pkg_check_modules(Dxflib GLOBAL IMPORTED_TARGET dxflib)
-endif()
-if(Dxflib_FOUND)
-  message(STATUS "Using system Dxflib (via pkg-config)")
-  add_library(Dxflib::Dxflib ALIAS PkgConfig::Dxflib)
+# Otherwise, try to find shared library on the system
+find_path(Dxflib_INCLUDE_DIR NAMES dl_dxf.h PATH_SUFFIXES dxflib)
+find_library(Dxflib_LIBRARY dxflib)
+if(Dxflib_INCLUDE_DIR AND Dxflib_LIBRARY)
+  add_library(Dxflib SHARED IMPORTED GLOBAL)
+  add_library(Dxflib::Dxflib ALIAS Dxflib)
+  set_property(TARGET Dxflib PROPERTY IMPORTED_LOCATION "${Dxflib_LIBRARY}")
+  target_include_directories(Dxflib INTERFACE "${Dxflib_INCLUDE_DIR}")
+  set(Dxflib_FOUND 1)
+  mark_as_advanced(Dxflib_INCLUDE_DIR Dxflib_LIBRARY)
+  message(STATUS "Using system Dxflib")
   return()
 endif()
 
+set(Dxflib_FOUND 0)
 message(FATAL_ERROR "Did not find Dxflib system library")
