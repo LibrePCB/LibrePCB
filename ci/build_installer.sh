d8ce3b94476a84bac0e44467f1a98a135ea1e1a7
U. Bruhin <urbibruhin@bluewin.ch>
2022-09-26 17:56:13 +0200

    Installer: Set version to "0.1.7-1"

--- ci/build_installer.sh
+++ ci/build_installer.sh
@@ -19,7 +19,7 @@ then
   EXECUTABLE_EXT="exe"
 fi
 TARGET="$OS-$ARCH"
-./dist/installer/update_metadata.sh "$TARGET" "0.1.6-1"  # TODO: How to determine version number?
+./dist/installer/update_metadata.sh "$TARGET" "0.1.7-1"  # TODO: How to determine version number?
 PACKAGES_DIR="./artifacts/installer_packages/$TARGET"
 mkdir -p $PACKAGES_DIR/librepcb.stable.app/data/stable
 cp -r ./dist/installer/output/packages/. $PACKAGES_DIR/
