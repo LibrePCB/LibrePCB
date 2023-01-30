53d7949bce486ac6e4d260d4b5b7e5ad23918a02
U. Bruhin <urbibruhin@bluewin.ch>
2023-01-30 16:16:00 +0100

    Support partially upgrading the latest unstable file format
    Allow partially upgrading previous unstable file format releases if the
    `LIBREPCB_UPGRADE_UNSTABLE` environment variable is set to `1`. This
    allows to upgrade our test data files and thus heavily simplifies
    developing file format upgrades.

---
+++ libs/librepcb/core/serialization/fileformatmigrationunstable.cpp
@@ -0,0 +1,111 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * https://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*******************************************************************************
+ *  Includes
+ ******************************************************************************/
+#include "fileformatmigrationunstable.h"
+
+#include "../application.h"
+
+#include <QtCore>
+
+/*******************************************************************************
+ *  Namespace
+ ******************************************************************************/
+namespace librepcb {
+
+/*******************************************************************************
+ *  Constructors / Destructor
+ ******************************************************************************/
+
+FileFormatMigrationUnstable::FileFormatMigrationUnstable(
+    QObject* parent) noexcept
+  : FileFormatMigrationV01(parent) {
+  mFromVersion = mToVersion;  // Clearly distinguish from the base class.
+}
+
+FileFormatMigrationUnstable::~FileFormatMigrationUnstable() noexcept {
+}
+
+/*******************************************************************************
+ *  General Methods
+ ******************************************************************************/
+
+void FileFormatMigrationUnstable::upgradeComponentCategory(
+    TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradePackageCategory(
+    TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradeSymbol(TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradePackage(TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradeComponent(
+    TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradeDevice(TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradeLibrary(TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+void FileFormatMigrationUnstable::upgradeWorkspaceData(
+    TransactionalDirectory& dir) {
+  Q_UNUSED(dir);
+}
+
+/*******************************************************************************
+ *  Private Methods
+ ******************************************************************************/
+
+void FileFormatMigrationUnstable::upgradeSchematic(LoadedData& data,
+                                                   SExpression& root) {
+  Q_UNUSED(data);
+  Q_UNUSED(root);
+}
+
+void FileFormatMigrationUnstable::upgradeBoard(SExpression& root,
+                                               QList<Message>& messages) {
+  Q_UNUSED(root);
+  Q_UNUSED(messages);
+}
+
+void FileFormatMigrationUnstable::upgradeBoardUserSettings(SExpression& root) {
+  Q_UNUSED(root);
+}
+
+/*******************************************************************************
+ *  End of File
+ ******************************************************************************/
+
+}  // namespace librepcb
