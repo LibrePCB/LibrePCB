orgTestCodacy11KRepos110MB/repo-4471-LibrePCB

LibraryElementCheck: Support serialization

libs/librepcb/core/library/msg/libraryelementcheckmessage.cpp
@@ -36,15 +36,19 @@ LibraryElementCheckMessage::LibraryElementCheckMessage(
   : mSeverity(other.mSeverity),
     mSeverityPixmap(other.mSeverityPixmap),
     mMessage(other.mMessage),
-    mDescription(other.mDescription) {
+    mDescription(other.mDescription),
+    mApproval(other.mApproval) {
 }
 
 LibraryElementCheckMessage::LibraryElementCheckMessage(
-    Severity severity, const QString& msg, const QString& description) noexcept
+    Severity severity, const QString& msg, const QString& description,
+    const QString& approvalName) noexcept
   : mSeverity(severity),
     mSeverityPixmap(getSeverityPixmap(severity)),
     mMessage(msg),
-    mDescription(description) {
+    mDescription(description),
+    mApproval(SExpression::createList("approved_message")) {
+  mApproval.appendChild(approvalName);
 }
 
 LibraryElementCheckMessage::~LibraryElementCheckMessage() noexcept {

LibrePCB/LibrePCB

libs/librepcb/core/library/msg/libraryelementcheckmessage.cpp
@@ -36,15 +36,19 @@ LibraryElementCheckMessage::LibraryElementCheckMessage(
   : mSeverity(other.mSeverity),
     mSeverityPixmap(other.mSeverityPixmap),
     mMessage(other.mMessage),
-    mDescription(other.mDescription) {
+    mDescription(other.mDescription),
+    mApproval(other.mApproval) {
 }
 
 LibraryElementCheckMessage::LibraryElementCheckMessage(
-    Severity severity, const QString& msg, const QString& description) noexcept
+    Severity severity, const QString& msg, const QString& description,
+    const QString& approvalName) noexcept
   : mSeverity(severity),
     mSeverityPixmap(getSeverityPixmap(severity)),
     mMessage(msg),
-    mDescription(description) {
+    mDescription(description),
+    mApproval(SExpression::createList("approval")) {
+  mApproval.appendChild(approvalName);
 }
 
 LibraryElementCheckMessage::~LibraryElementCheckMessage() noexcept {