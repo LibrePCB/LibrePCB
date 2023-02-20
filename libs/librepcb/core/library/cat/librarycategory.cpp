orgTestCodacy11KRepos110MB/repo-4471-LibrePCB

Library elements: Load/store approved messages

libs/librepcb/core/library/cat/librarycategory.cpp
@@ -67,6 +67,8 @@ void LibraryCategory::serialize(SExpression& root) const {
   root.ensureLineBreak();
   root.appendChild("parent", mParentUuid);
   root.ensureLineBreak();
+  serializeApprovedMessages(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/cmp/component.cpp
@@ -151,6 +151,8 @@ void Component::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mSymbolVariants.serialize(root);
   root.ensureLineBreak();
+  serializeApprovedMessages(root);
+  root.ensureLineBreak();
 }
 
 QString Component::cleanNorm(QString norm) noexcept {

libs/librepcb/core/library/dev/device.cpp
@@ -103,6 +103,8 @@ void Device::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mPadSignalMap.sortedByUuid().serialize(root);
   root.ensureLineBreak();
+  serializeApprovedMessages(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/library.cpp
@@ -179,6 +179,8 @@ void Library::serialize(SExpression& root) const {
     root.appendChild("dependency", uuid);
   }
   root.ensureLineBreak();
+  serializeApprovedMessages(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/pkg/package.cpp
@@ -76,6 +76,8 @@ void Package::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mFootprints.serialize(root);
   root.ensureLineBreak();
+  serializeApprovedMessages(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/sym/symbol.cpp
@@ -204,6 +204,8 @@ void Symbol::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mTexts.serialize(root);
   root.ensureLineBreak();
+  serializeApprovedMessages(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

LibrePCB/LibrePCB

libs/librepcb/core/library/cat/librarycategory.cpp
@@ -64,6 +64,8 @@ void LibraryCategory::serialize(SExpression& root) const {
   root.ensureLineBreak();
   root.appendChild("parent", mParentUuid);
   root.ensureLineBreak();
+  serializeMessageApprovals(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/cmp/component.cpp
@@ -162,6 +162,8 @@ void Component::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mSymbolVariants.serialize(root);
   root.ensureLineBreak();
+  serializeMessageApprovals(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/dev/device.cpp
@@ -119,6 +119,8 @@ void Device::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mPadSignalMap.sortedByUuid().serialize(root);
   root.ensureLineBreak();
+  serializeMessageApprovals(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/library.cpp
@@ -197,6 +197,8 @@ void Library::serialize(SExpression& root) const {
     root.appendChild("dependency", uuid);
   }
   root.ensureLineBreak();
+  serializeMessageApprovals(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/pkg/package.cpp
@@ -93,6 +93,8 @@ void Package::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mFootprints.serialize(root);
   root.ensureLineBreak();
+  serializeMessageApprovals(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************

libs/librepcb/core/library/sym/symbol.cpp
@@ -120,6 +120,8 @@ void Symbol::serialize(SExpression& root) const {
   root.ensureLineBreak();
   mTexts.serialize(root);
   root.ensureLineBreak();
+  serializeMessageApprovals(root);
+  root.ensureLineBreak();
 }
 
 /*******************************************************************************