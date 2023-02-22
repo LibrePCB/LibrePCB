orgTestCodacy11KRepos110MB/repo-4471-LibrePCB

Library editor: Support approving check messages

libs/librepcb/editor/library/cat/packagecategoryeditorwidget.cpp
@@ -99,6 +99,10 @@ PackageCategoryEditorWidget::~PackageCategoryEditorWidget() noexcept {
  ******************************************************************************/
 
 bool PackageCategoryEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mCategory->setApprovedMessages(mCategory->getApprovedMessages() -
+                                 mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -130,6 +134,7 @@ void PackageCategoryEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mCategory->getAuthor());
   mUi->edtVersion->setText(mCategory->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mCategory->isDeprecated());
+  mUi->lstMessages->setApprovals(mCategory->getApprovedMessages());
   mParentUuid = mCategory->getParentUuid();
   updateCategoryLabel();
 }
@@ -202,6 +207,13 @@ bool PackageCategoryEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgNameNotTitleCase>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgMissingAuthor>(msg, applyFix)) return true;
   return false;
+}
+
+void PackageCategoryEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mCategory, msg, approve);
+  updateMetadata();
 }
 
 void PackageCategoryEditorWidget::btnChooseParentCategoryClicked() noexcept {

libs/librepcb/editor/library/cmp/componenteditorwidget.cpp
@@ -139,6 +139,10 @@ ComponentEditorWidget::~ComponentEditorWidget() noexcept {
  ******************************************************************************/
 
 bool ComponentEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mComponent->setApprovedMessages(mComponent->getApprovedMessages() -
+                                  mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -171,6 +175,7 @@ void ComponentEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mComponent->getAuthor());
   mUi->edtVersion->setText(mComponent->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mComponent->isDeprecated());
+  mUi->lstMessages->setApprovals(mComponent->getApprovedMessages());
   mCategoriesEditorWidget->setUuids(mComponent->getCategories());
   mUi->cbxSchematicOnly->setChecked(mComponent->isSchematicOnly());
   mUi->edtPrefix->setText(*mComponent->getPrefixes().getDefaultValue());
@@ -337,6 +342,13 @@ bool ComponentEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgMissingComponentDefaultValue>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgMissingSymbolVariant>(msg, applyFix)) return true;
   return false;
+}
+
+void ComponentEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mComponent, msg, approve);
+  updateMetadata();
 }
 
 /*******************************************************************************

libs/librepcb/editor/library/lib/libraryoverviewwidget.cpp
@@ -172,6 +172,10 @@ void LibraryOverviewWidget::setFilter(const QString& filter) noexcept {
  ******************************************************************************/
 
 bool LibraryOverviewWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mLibrary->setApprovedMessages(mLibrary->getApprovedMessages() -
+                                mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -224,6 +228,7 @@ void LibraryOverviewWidget::updateMetadata() noexcept {
   mUi->edtVersion->setText(mLibrary->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mLibrary->isDeprecated());
   mUi->edtUrl->setText(mLibrary->getUrl().toString());
+  mUi->lstMessages->setApprovals(mLibrary->getApprovedMessages());
   mDependenciesEditorWidget->setUuids(mLibrary->getDependencies());
   mIcon = mLibrary->getIcon();
 }
@@ -297,6 +302,13 @@ bool LibraryOverviewWidget::processCheckMessage(
   if (fixMsgHelper<MsgNameNotTitleCase>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgMissingAuthor>(msg, applyFix)) return true;
   return false;
+}
+
+void LibraryOverviewWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mLibrary, msg, approve);
+  updateMetadata();
 }
 
 void LibraryOverviewWidget::updateElementLists() noexcept {

libs/librepcb/editor/library/pkg/packageeditorwidget.cpp
@@ -214,6 +214,10 @@ void PackageEditorWidget::setStatusBar(StatusBar* statusbar) noexcept {
  ******************************************************************************/
 
 bool PackageEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mPackage->setApprovedMessages(mPackage->getApprovedMessages() -
+                                mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -318,6 +322,7 @@ void PackageEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mPackage->getAuthor());
   mUi->edtVersion->setText(mPackage->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mPackage->isDeprecated());
+  mUi->lstMessages->setApprovals(mPackage->getApprovedMessages());
   mCategoriesEditorWidget->setUuids(mPackage->getCategories());
 }
 
@@ -532,6 +537,13 @@ bool PackageEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgMissingFootprintValue>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgWrongFootprintTextLayer>(msg, applyFix)) return true;
   return false;
+}
+
+void PackageEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mPackage, msg, approve);
+  updateMetadata();
 }
 
 bool PackageEditorWidget::execGraphicsExportDialog(

libs/librepcb/editor/library/sym/symboleditorwidget.cpp
@@ -201,6 +201,10 @@ void SymbolEditorWidget::setStatusBar(StatusBar* statusbar) noexcept {
  ******************************************************************************/
 
 bool SymbolEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mSymbol->setApprovedMessages(mSymbol->getApprovedMessages() -
+                               mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -301,6 +305,7 @@ void SymbolEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mSymbol->getAuthor());
   mUi->edtVersion->setText(mSymbol->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mSymbol->isDeprecated());
+  mUi->lstMessages->setApprovals(mSymbol->getApprovedMessages());
   mCategoriesEditorWidget->setUuids(mSymbol->getCategories());
 }
 
@@ -491,6 +496,13 @@ bool SymbolEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgWrongSymbolTextLayer>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgSymbolPinNotOnGrid>(msg, applyFix)) return true;
   return false;
+}
+
+void SymbolEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mSymbol, msg, approve);
+  updateMetadata();
 }
 
 bool SymbolEditorWidget::execGraphicsExportDialog(

LibrePCB/LibrePCB

libs/librepcb/editor/library/cat/packagecategoryeditorwidget.cpp
@@ -110,6 +110,10 @@ QSet<EditorWidgetBase::Feature>
  ******************************************************************************/
 
 bool PackageCategoryEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mCategory->setMessageApprovals(mCategory->getMessageApprovals() -
+                                 mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -141,6 +145,7 @@ void PackageCategoryEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mCategory->getAuthor());
   mUi->edtVersion->setText(mCategory->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mCategory->isDeprecated());
+  mUi->lstMessages->setApprovals(mCategory->getMessageApprovals());
   mParentUuid = mCategory->getParentUuid();
   updateCategoryLabel();
 }
@@ -213,6 +218,13 @@ bool PackageCategoryEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgNameNotTitleCase>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgMissingAuthor>(msg, applyFix)) return true;
   return false;
+}
+
+void PackageCategoryEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mCategory, msg, approve);
+  updateMetadata();
 }
 
 void PackageCategoryEditorWidget::btnChooseParentCategoryClicked() noexcept {

libs/librepcb/editor/library/cmp/componenteditorwidget.cpp
@@ -151,6 +151,10 @@ QSet<EditorWidgetBase::Feature> ComponentEditorWidget::getAvailableFeatures()
  ******************************************************************************/
 
 bool ComponentEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mComponent->setMessageApprovals(mComponent->getMessageApprovals() -
+                                  mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -183,6 +187,7 @@ void ComponentEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mComponent->getAuthor());
   mUi->edtVersion->setText(mComponent->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mComponent->isDeprecated());
+  mUi->lstMessages->setApprovals(mComponent->getMessageApprovals());
   mCategoriesEditorWidget->setUuids(mComponent->getCategories());
   mUi->cbxSchematicOnly->setChecked(mComponent->isSchematicOnly());
   mUi->edtPrefix->setText(*mComponent->getPrefixes().getDefaultValue());
@@ -349,6 +354,13 @@ bool ComponentEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgMissingComponentDefaultValue>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgMissingSymbolVariant>(msg, applyFix)) return true;
   return false;
+}
+
+void ComponentEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mComponent, msg, approve);
+  updateMetadata();
 }
 
 /*******************************************************************************

libs/librepcb/editor/library/lib/libraryoverviewwidget.cpp
@@ -194,6 +194,10 @@ void LibraryOverviewWidget::setFilter(const QString& filter) noexcept {
  ******************************************************************************/
 
 bool LibraryOverviewWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mLibrary->setMessageApprovals(mLibrary->getMessageApprovals() -
+                                mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -296,6 +300,7 @@ void LibraryOverviewWidget::updateMetadata() noexcept {
   mUi->edtVersion->setText(mLibrary->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mLibrary->isDeprecated());
   mUi->edtUrl->setText(mLibrary->getUrl().toString());
+  mUi->lstMessages->setApprovals(mLibrary->getMessageApprovals());
   mDependenciesEditorWidget->setUuids(mLibrary->getDependencies());
   mIcon = mLibrary->getIcon();
 }
@@ -369,6 +374,13 @@ bool LibraryOverviewWidget::processCheckMessage(
   if (fixMsgHelper<MsgNameNotTitleCase>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgMissingAuthor>(msg, applyFix)) return true;
   return false;
+}
+
+void LibraryOverviewWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mLibrary, msg, approve);
+  updateMetadata();
 }
 
 void LibraryOverviewWidget::updateElementLists() noexcept {

libs/librepcb/editor/library/pkg/packageeditorwidget.cpp
@@ -267,6 +267,10 @@ void PackageEditorWidget::disconnectEditor() noexcept {
  ******************************************************************************/
 
 bool PackageEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mPackage->setMessageApprovals(mPackage->getMessageApprovals() -
+                                mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -391,6 +395,7 @@ void PackageEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mPackage->getAuthor());
   mUi->edtVersion->setText(mPackage->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mPackage->isDeprecated());
+  mUi->lstMessages->setApprovals(mPackage->getMessageApprovals());
   mCategoriesEditorWidget->setUuids(mPackage->getCategories());
 }
 
@@ -643,6 +648,13 @@ bool PackageEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgUnusedCustomPadOutline>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgInvalidCustomPadOutline>(msg, applyFix)) return true;
   return false;
+}
+
+void PackageEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mPackage, msg, approve);
+  updateMetadata();
 }
 
 bool PackageEditorWidget::execGraphicsExportDialog(

libs/librepcb/editor/library/sym/symboleditorwidget.cpp
@@ -252,6 +252,10 @@ void SymbolEditorWidget::disconnectEditor() noexcept {
  ******************************************************************************/
 
 bool SymbolEditorWidget::save() noexcept {
+  // Remove obsolete message approvals (bypassing the undo stack).
+  mSymbol->setMessageApprovals(mSymbol->getMessageApprovals() -
+                               mDisappearedApprovals);
+
   // Commit metadata.
   QString errorMsg = commitMetadata();
   if (!errorMsg.isEmpty()) {
@@ -372,6 +376,7 @@ void SymbolEditorWidget::updateMetadata() noexcept {
   mUi->edtAuthor->setText(mSymbol->getAuthor());
   mUi->edtVersion->setText(mSymbol->getVersion().toStr());
   mUi->cbxDeprecated->setChecked(mSymbol->isDeprecated());
+  mUi->lstMessages->setApprovals(mSymbol->getMessageApprovals());
   mCategoriesEditorWidget->setUuids(mSymbol->getCategories());
 }
 
@@ -576,6 +581,13 @@ bool SymbolEditorWidget::processCheckMessage(
   if (fixMsgHelper<MsgWrongSymbolTextLayer>(msg, applyFix)) return true;
   if (fixMsgHelper<MsgSymbolPinNotOnGrid>(msg, applyFix)) return true;
   return false;
+}
+
+void SymbolEditorWidget::libraryElementCheckApproveRequested(
+    std::shared_ptr<const LibraryElementCheckMessage> msg,
+    bool approve) noexcept {
+  setMessageApproved(*mSymbol, msg, approve);
+  updateMetadata();
 }
 
 bool SymbolEditorWidget::execGraphicsExportDialog(