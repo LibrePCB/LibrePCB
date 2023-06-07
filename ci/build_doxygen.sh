8170686f4a841232036d54551a19138e1a8cddc2
U. Bruhin <urbibruhin@bluewin.ch>
2023-06-07 22:45:24 +0200

    CI: Don't upload Qt helpfile from Doxygen
    It exceeds the maximum upload size configured on the server and is not
    needed anyway.

--- ci/build_doxygen.sh
+++ ci/build_doxygen.sh
@@ -7,5 +7,8 @@ pushd ./dev/doxygen
 ./make.sh -Werror
 popd
 
+# Remove Qt helpfile to heavily reduce the upload file size.
+rm ./dev/doxygen/output/*.qch
+
 mkdir -p ./artifacts
 cp -r ./dev/doxygen/output/. ./artifacts/doxygen/
