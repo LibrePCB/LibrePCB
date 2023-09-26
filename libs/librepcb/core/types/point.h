5b92bd2c8240ce664bf0c76d6e62b05914d758d9
Frantisek Burian <BuFran@seznam.cz>
2023-09-26 12:56:44 +0200

    Use std::hypot() for Point::getLength() calculation (fixes issue LibrePCB#1204)

--- libs/librepcb/core/types/point.h
+++ libs/librepcb/core/types/point.h
@@ -210,7 +210,7 @@ public:
    */
   UnsignedLength getLength() const noexcept {
     LengthBase_t length = static_cast<LengthBase_t>(
-        qSqrt(mX.toNm() * mX.toNm() + mY.toNm() * mY.toNm()));
+        std::hypot(mX.toNm(), mY.toNm()));
     Q_ASSERT(length >= 0);
     return UnsignedLength(length);
   }
