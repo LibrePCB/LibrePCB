767585a40c16393398ee84f9e6d3b3ffadbeff9f
U. Bruhin <urbibruhin@bluewin.ch>
2023-12-12 15:07:17 +0700

    WIP

---
+++ libs/librepcb/gui/opengllayer.h
@@ -0,0 +1,15 @@
+#ifndef LIBREPCB_GUI_OPENGLLAYER_H
+#define LIBREPCB_GUI_OPENGLLAYER_H
+
+namespace librepcb {
+namespace gui {
+
+class OpenGlLayer {
+public:
+  OpenGlLayer();
+};
+
+}  // namespace gui
+}  // namespace librepcb
+
+#endif
