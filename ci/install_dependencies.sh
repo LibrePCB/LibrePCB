bb11af2f24b96a73cd72af5b2d03a86753d064d2
U. Bruhin <urbibruhin@bluewin.ch>
2024-03-07 12:16:40 +0100

    CI: Install imageformats plugin for deployment

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -5,6 +5,21 @@ set -euv -o pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Install the Qt image formats plugin since it is not yet available in our
+# Docker images. Needs to be removed when added to the images.
+if [ "$OS" = "windows" ]
+then
+  QT_IMAGEFORMATS_URL="https://download.qt.io/online/qtsdkrepository/windows_x86/desktop/qt5_5152/qt.qt5.5152.win32_mingw81/5.15.2-0-202011130602qtimageformats-Windows-Windows_7-Mingw-Windows-Windows_7-X86.7z"
+  powershell -Command "Invoke-WebRequest $QT_IMAGEFORMATS_URL -OutFile 'C:/tmp.7z' -UseBasicParsing ;" \
+    && 7z x C:/tmp.7z -oC:/Qt -bsp1 \
+    && rm C:/tmp.7z
+elif [ "$OS" = "linux" ] && [ "${DEPLOY-}" = "true" ]
+then
+  QT_IMAGEFORMATS_URL="https://download.qt.io/online/qtsdkrepository/linux_x64/desktop/qt5_5152/qt.qt5.5152.gcc_64/5.15.2-0-202011130601qtimageformats-Linux-RHEL_7_6-GCC-Linux-RHEL_7_6-X86_64.7z"
+  wget -c "$QT_IMAGEFORMATS_URL" -O /tmp/tmp.7z
+  /tmp/docker exec -t -u root mycontainer 7za x /tmp/tmp.7z -o/opt/qt
+fi
+
 # On MacOS, we have to install some dependencies since we can't use a custom
 # Docker container with all tools preinstalled.
 if [ "$OS" = "mac" ]
