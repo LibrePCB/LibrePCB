c2461882e71baa40a11254eeba0425a35fa60af8
U. Bruhin <urbibruhin@bluewin.ch>
2024-12-25 11:00:39 +0100

    Implement ZipArchive in Rust with C++ wrapper

---
+++ libs/librepcb/core/fileio/ziparchive.cpp
@@ -0,0 +1,104 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * https://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*******************************************************************************
+ *  Includes
+ ******************************************************************************/
+#include "ziparchive.h"
+
+#include "../exceptions.h"
+#include "filepath.h"
+
+#include <librepcb/rust-core/ffi.h>
+
+#include <QtCore>
+
+/*******************************************************************************
+ *  Namespace
+ ******************************************************************************/
+namespace librepcb {
+
+/*******************************************************************************
+ *  Class ZipArchive
+ ******************************************************************************/
+
+RustHandle<rs::ZipArchive> construct(const QByteArray& data) {
+  QString err;
+  if (auto obj = rs::ffi_ziparchive_new_from_mem(&data, &err)) {
+    return RustHandle<rs::ZipArchive>(*obj, &rs::ffi_ziparchive_delete);
+  } else {
+    throw RuntimeError(__FILE__, __LINE__, "Failed to open Zip file: " % err);
+  }
+}
+
+static RustHandle<rs::ZipArchive> construct(const FilePath& fp) {
+  const QString path = fp.toStr();
+  QString err;
+  if (auto obj = rs::ffi_ziparchive_new_from_file(&path, &err)) {
+    return RustHandle<rs::ZipArchive>(*obj, &rs::ffi_ziparchive_delete);
+  } else {
+    throw RuntimeError(
+        __FILE__, __LINE__,
+        QString("Failed to open Zip file '%1': %2").arg(fp.toNative(), err));
+  }
+}
+
+ZipArchive::ZipArchive(const QByteArray& data) : mHandle(construct(data)) {
+}
+
+ZipArchive::ZipArchive(const FilePath& fp) : mHandle(construct(fp)) {
+}
+
+std::size_t ZipArchive::getEntriesCount() const noexcept {
+  return rs::ffi_ziparchive_len(*mHandle);
+}
+
+QString ZipArchive::getFileName(std::size_t index) {
+  QString name;
+  QString err;
+  if (!rs::ffi_ziparchive_name_for_index(*mHandle, index, &name, &err)) {
+    throw RuntimeError(__FILE__, __LINE__,
+                       "Failed to get file name from Zip: " % err);
+  }
+  return name;
+}
+
+QByteArray ZipArchive::readFile(std::size_t index) {
+  QByteArray buf;
+  QString err;
+  if (!rs::ffi_ziparchive_read_by_index(*mHandle, index, &buf, &err)) {
+    throw RuntimeError(__FILE__, __LINE__, "Failed to read from Zip: " % err);
+  }
+  return buf;
+}
+
+void ZipArchive::extractTo(const FilePath& dir) {
+  const QString path = dir.toStr();
+  if (!rs::ffi_ziparchive_extract(*mHandle, &path)) {
+    throw RuntimeError(
+        __FILE__, __LINE__,
+        QString("Failed to extract Zip archive to '%1'.").arg(dir.toNative()));
+  }
+}
+
+/*******************************************************************************
+ *  End of File
+ ******************************************************************************/
+
+}  // namespace librepcb
---
+++ libs/librepcb/core/fileio/ziparchive.h
@@ -0,0 +1,124 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * https://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBREPCB_CORE_ZIPARCHIVE_H
+#define LIBREPCB_CORE_ZIPARCHIVE_H
+
+/*******************************************************************************
+ *  Includes
+ ******************************************************************************/
+#include "../utils/rusthandle.h"
+
+#include <QtCore>
+
+/*******************************************************************************
+ *  Namespace / Forward Declarations
+ ******************************************************************************/
+namespace librepcb {
+
+class FilePath;
+
+namespace rs {
+struct ZipArchive;
+}
+
+/*******************************************************************************
+ *  Class ZipArchive
+ ******************************************************************************/
+
+/**
+ * @brief Zip file reader
+ *
+ * @note This is just a wrapper around its Rust implementation.
+ */
+class ZipArchive final {
+public:
+  ZipArchive() = delete;
+  ZipArchive(const ZipArchive& other) = delete;
+  ZipArchive& operator=(const ZipArchive& rhs) = delete;
+
+  /**
+   * @brief Open in-memory Zip from `QByteArray`
+   *
+   * @param data  Raw Zip content.
+   *
+   * @throws Exception on any error
+   */
+  ZipArchive(const QByteArray& data);
+
+  /**
+   * @brief Open Zip file
+   *
+   * @param fp    Path to Zip file.
+   *
+   * @throws Exception on any error
+   */
+  ZipArchive(const FilePath& fp);
+
+  /**
+   * @brief Get number of ZIp entries (files & directories)
+   *
+   * @return Number of entries
+   */
+  std::size_t getEntriesCount() const noexcept;
+
+  /**
+   * @brief Get the file name of an entry
+   *
+   * @param index Entry index
+   *
+   * @return Entry file name (directory if it ends with slash or backslash)
+   *
+   * @throws  On invalid index or if the entry file name is somehow invalid
+   *          or dangerous (zeros in its name, path outside Zip, ...).
+   */
+  QString getFileName(std::size_t index);
+
+  /**
+   * @brief Read the content of a file
+   *
+   * @param index Entry index
+   *
+   * @return File content
+   *
+   * @throws On invalid index or I/O errors.
+   */
+  QByteArray readFile(std::size_t index);
+
+  /**
+   * @brief Extract whole Zip to a directory (overwriting)
+   *
+   * @param dir   Target directory
+   *
+   * @throws  On invalid/unsafe Zip content or I/O errors. In that case, the
+   *          extraction might have been only partially (not atomically).
+   */
+  void extractTo(const FilePath& dir);
+
+private:
+  RustHandle<rs::ZipArchive> mHandle;
+};
+
+/*******************************************************************************
+ *  End of File
+ ******************************************************************************/
+
+}  // namespace librepcb
+
+#endif
