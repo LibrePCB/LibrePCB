adbcf3af373545f00f4ec45a4d198374ddda0af1
Fred Turkington <z3ugma@users.noreply.github.com>
2025-06-19 14:33:15 -0500

    CLI - add unit tests for open-package and open-symbol
    Addresses #1530

---
+++ tests/cli/open_symbol/test_parser.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Test command "open-symbol" (basic parser tests)
+"""
+
+HELP_TEXT = """\
+Usage: {executable} [options] open-symbol [command_options] symbol
+LibrePCB Command Line Interface
+
+Options:
+  -h, --help       Print this message.
+  -V, --version    Displays version information.
+  -v, --verbose    Verbose output.
+  --check          Run the symbol check, print all non-approved messages and
+                   report failure (exit code = 1) if there are non-approved
+                   messages.
+  --export <file>  Export the symbol to a graphical file. Supported file
+                   extensions: pdf, svg, bmp, cur, heic, heif, icns, ico, jfif,
+                   jp2, jpeg, jpg, pbm, pgm, png, ppm, tif, tiff, wbmp, webp,
+                   xbm, xpm
+
+Arguments:
+  open-symbol      Open a symbol to execute symbol-related tasks.
+  symbol           Path to symbol directory (containing *.lp).
+"""
+
+ERROR_TEXT = """\
+{error}
+Usage: {executable} [options] open-symbol [command_options] symbol
+Help: {executable} open-symbol --help
+"""
+
+
+def test_help(cli):
+    code, stdout, stderr = cli.run("open-symbol", "--help")
+    assert stderr == ""
+    assert stdout == HELP_TEXT.format(executable=cli.executable)
+    assert code == 0
+
+
+def test_no_arguments(cli):
+    code, stdout, stderr = cli.run("open-symbol")
+    assert stderr == ERROR_TEXT.format(
+        executable=cli.executable,
+        error="Missing arguments: symbol",
+    )
+    assert stdout == ""
+    assert code == 1
+
+
+def test_invalid_argument(cli):
+    code, stdout, stderr = cli.run("open-symbol", "--invalid-argument")
+    assert stderr == ERROR_TEXT.format(
+        executable=cli.executable,
+        error="Unknown option 'invalid-argument'.",
+    )
+    assert stdout == ""
+    assert code == 1
