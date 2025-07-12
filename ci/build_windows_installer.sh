132a79f37a163b7e891be55291451e0c410ddf22
U. Bruhin <urbibruhin@bluewin.ch>
2025-07-12 16:26:19 +0200

    CI: Setup code signing of Windows executables

--- ci/build_windows_installer.sh
+++ ci/build_windows_installer.sh
@@ -8,13 +8,24 @@ set -euv -o pipefail
 # we adjust PATH.
 export PATH="/usr/bin:$PATH"
 
+# Enable code signing only when available (detected by AST_TENANT).
+if [[ -n "${AST_TENANT-}" ]]
+then
+  echo "Code signing is enabled"
+  SIGN_FLAG="//DSIGN"
+else
+  echo "Code signing is disabled"
+  SIGN_FLAG=""
+fi
+
 # Build the installer.
 OUTPUT_DIRECTORY=".\\artifacts\\nightly_builds"
 OUTPUT_BASENAME="librepcb-installer-nightly-$OS-$ARCH"
 cp -r ./dist/innosetup/* ./build/dist/innosetup/
 cp -r ./build/install/. ./build/dist/innosetup/files
 iscc ./build/dist/innosetup/installer.iss \
-  //O"$OUTPUT_DIRECTORY" //F"$OUTPUT_BASENAME"
+  //O"$OUTPUT_DIRECTORY" //F"$OUTPUT_BASENAME" \
+  $SIGN_FLAG //Ssigntool="powershell.exe -File $(cygpath -aw ci/sign.ps1) \$f"
 
 # Print checksum to allow fully transparent public verification.
 sha256sum "$OUTPUT_DIRECTORY\\$OUTPUT_BASENAME.exe"
