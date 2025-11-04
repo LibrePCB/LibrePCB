15ab4c67319a257167eab27b5dc9e6c4b845f5b0
U. Bruhin <urbibruhin@bluewin.ch>
2025-11-04 23:41:43 +0100

    Add new library element type "Corporate"

---
+++ libs/librepcb/core/library/corp/corporatecheck.cpp
@@ -0,0 +1,59 @@
+/*
+ * LibrePCB - Professional EDA for everyone!
+ * Copyright (C) 2013 LibrePCB Developers, see AUTHORS.md for contributors.
+ * https://librepcb.org/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*******************************************************************************
+ *  Includes
+ ******************************************************************************/
+#include "corporatecheck.h"
+
+#include "corporate.h"
+
+#include <QtCore>
+
+/*******************************************************************************
+ *  Namespace
+ ******************************************************************************/
+namespace librepcb {
+
+/*******************************************************************************
+ *  Constructors / Destructor
+ ******************************************************************************/
+
+CorporateCheck::CorporateCheck(const Corporate& Corporate) noexcept
+  : LibraryBaseElementCheck(Corporate), mCorporate(Corporate) {
+}
+
+CorporateCheck::~CorporateCheck() noexcept {
+}
+
+/*******************************************************************************
+ *  General Methods
+ ******************************************************************************/
+
+RuleCheckMessageList CorporateCheck::runChecks() const {
+  RuleCheckMessageList msgs = LibraryBaseElementCheck::runChecks();
+  Q_UNUSED(mCorporate);
+  return msgs;
+}
+
+/*******************************************************************************
+ *  End of File
+ ******************************************************************************/
+
+}  // namespace librepcb
