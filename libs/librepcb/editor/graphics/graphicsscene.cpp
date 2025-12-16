60c7cd403b485d04d3b3659e8587bdb0ecc7213c
U. Bruhin <urbibruhin@bluewin.ch>
2025-12-16 12:34:19 +0100

    Automatically switch to 10x grid when zooming out

--- libs/librepcb/editor/graphics/graphicsscene.cpp
+++ libs/librepcb/editor/graphics/graphicsscene.cpp
@@ -203,40 +203,41 @@ void GraphicsScene::drawBackground(QPainter* painter,
   gridPen.setWidth((mGridStyle == Theme::GridStyle::Dots) ? 2 : 1);
   painter->setPen(gridPen);
   painter->setBrush(Qt::NoBrush);
-  const qreal gridIntervalPixels = mGridInterval->toPx();
   const qreal lod = QStyleOptionGraphicsItem::levelOfDetailFromTransform(
       painter->worldTransform());
-  if (gridIntervalPixels * lod >= 6) {
-    qreal left, right, top, bottom;
-    left = qFloor(rect.left() / gridIntervalPixels) * gridIntervalPixels;
-    right = rect.right();
-    top = rect.top();
-    bottom = qFloor(rect.bottom() / gridIntervalPixels) * gridIntervalPixels;
-    switch (mGridStyle) {
-      case Theme::GridStyle::Lines: {
-        QVarLengthArray<QLineF, 500> lines;
-        for (qreal x = left; x < right; x += gridIntervalPixels)
-          lines.append(QLineF(x, rect.top(), x, rect.bottom()));
-        for (qreal y = bottom; y > top; y -= gridIntervalPixels)
-          lines.append(QLineF(rect.left(), y, rect.right(), y));
-        painter->setOpacity(0.5);
-        painter->drawLines(lines.data(), lines.size());
-        painter->setOpacity(1);
-        break;
-      }
-
-      case Theme::GridStyle::Dots: {
-        QVarLengthArray<QPointF, 2000> dots;
-        for (qreal x = left; x < right; x += gridIntervalPixels)
-          for (qreal y = bottom; y > top; y -= gridIntervalPixels)
-            dots.append(QPointF(x, y));
-        painter->drawPoints(dots.data(), dots.size());
-        break;
-      }
-
-      default:
-        break;
+  qreal gridIntervalPixels = mGridInterval->toPx();
+  while (gridIntervalPixels * lod < 7) {
+    gridIntervalPixels *= 10;
+  }
+  qreal left, right, top, bottom;
+  left = qFloor(rect.left() / gridIntervalPixels) * gridIntervalPixels;
+  right = rect.right();
+  top = rect.top();
+  bottom = qFloor(rect.bottom() / gridIntervalPixels) * gridIntervalPixels;
+  switch (mGridStyle) {
+    case Theme::GridStyle::Lines: {
+      QVarLengthArray<QLineF, 500> lines;
+      for (qreal x = left; x < right; x += gridIntervalPixels)
+        lines.append(QLineF(x, rect.top(), x, rect.bottom()));
+      for (qreal y = bottom; y > top; y -= gridIntervalPixels)
+        lines.append(QLineF(rect.left(), y, rect.right(), y));
+      painter->setOpacity(0.5);
+      painter->drawLines(lines.data(), lines.size());
+      painter->setOpacity(1);
+      break;
     }
+
+    case Theme::GridStyle::Dots: {
+      QVarLengthArray<QPointF, 2000> dots;
+      for (qreal x = left; x < right; x += gridIntervalPixels)
+        for (qreal y = bottom; y > top; y -= gridIntervalPixels)
+          dots.append(QPointF(x, y));
+      painter->drawPoints(dots.data(), dots.size());
+      break;
+    }
+
+    default:
+      break;
   }
 }
 
