690a4e7a79fc30af4a5a87bc6a31c5c54ae6fdae
U. Bruhin <urbibruhin@bluewin.ch>
2026-09-07 09:43:14 +0200

    CI: Stop using Homebrew, get dependencies from upstream (#1886)
    Homebrew doesn't provide bottles for UV anymore, thus now installing
    it directly from Astral. Also Ninja and Rustup are actually already
    available on the base system, so we can use those instead of relying
    on Homebrew. And the remaining tools were easy to install from their
    upstream tools too, so we could get rid of Homebrew completely.

--- ci/install_dependencies.sh
+++ ci/install_dependencies.sh
@@ -7,56 +7,56 @@ set -euv -o pipefail
 # Docker container with all tools preinstalled.
 if [ "$OS" = "mac" ]
 then
-  # Update homebrow to avoid issues due to outdated package database. But
-  # because even the update sometimes fails, let's ignore any errors with
-  # "|| true" (Apple-style error handling). Maybe this way we get successful
-  # builds even if homebrow failed, which saves a lot of time and nerves.
-  echo "Updating package database..."
-  brew update || true
-
-  # Actually we don't want to waste time with upgrading packages, but sometimes
-  # this needs to be done to get it working (maybe it depends on the phase of
-  # the moon whether this is required or not).
-  echo "Upgrading packages..."
-  brew upgrade || true
+  # Important: Do not install any packages from Homebrew! It has caused a lot
+  # of headaches in the past - life is better without it.
 
   # Install create-dmg
   echo "Installing create-dmg..."
-  brew install --force-bottle create-dmg
+  mkdir -p "$RUNNER_TEMP/create-dmg"
+  curl -L "https://github.com/create-dmg/create-dmg/archive/a2b71d0fda6d0df2a86dc7f67082d4d73e84c59f.tar.gz" \
+    |  tar -xz --strip-components=1 -C "$RUNNER_TEMP/create-dmg"
+  make -j$(nproc) -C "$RUNNER_TEMP/create-dmg" prefix="$HOME/.local" install
 
   # Install dylibbundler
   echo "Installing dylibbundler..."
-  brew install --force-bottle dylibbundler
-
-  # Install Ninja
-  echo "Installing ninja..."
-  brew install --force-bottle ninja
+  mkdir -p "$RUNNER_TEMP/dylibbundler"
+  curl -L "https://github.com/auriamg/macdylibbundler/archive/63105a5571e0e9a83a8f2c37d0b91f2398b2031b.tar.gz" \
+    | tar -xz --strip-components=1 -C "$RUNNER_TEMP/dylibbundler"
+  make -j$(nproc) -C "$RUNNER_TEMP/dylibbundler" PREFIX="$HOME/.local" install
 
   # Install Rust toolchain
   # IMPORTANT: Rust from homebrew contains a stdlib that does not run on older
   # macOS versions, therefore we use rustup to install an official build!
   echo "Installing Rust toolchain..."
-  brew install --force-bottle rustup
   rustup install --profile minimal 1.96.0
   rustup default 1.96.0
-  export PATH="$(brew --prefix rustup)/bin:$PATH"
+  export PATH="$CARGO_HOME/bin:$PATH"
 
   # Install Cargo packages
   cargo install --force cargo-llvm-cov
 
   # Install ccache
   echo "Installing ccache..."
-  brew install --force-bottle ccache
+  mkdir -p "$RUNNER_TEMP/ccache"
+  curl -o "$RUNNER_TEMP/ccache.tar.gz" -L \
+    "https://github.com/ccache/ccache/releases/download/v4.14/ccache-4.14-darwin.tar.gz"
+  shasum -a 256 --check <<< \
+    "353a81ea8680d93387102cfde288ebed381a54272cfdc18224f241add6332b39  $RUNNER_TEMP/ccache.tar.gz"
+  tar -xf "$RUNNER_TEMP/ccache.tar.gz" --strip-components=1 -C "$RUNNER_TEMP/ccache"
+  "$RUNNER_TEMP/ccache/install.sh" --prefix="$HOME/.local"
 
   # Install uv
   echo "Installing uv..."
-  brew install --force-bottle uv
+  curl -o "$RUNNER_TEMP/uv.tar.gz" -L "$UV_URL"
+  shasum -a 256 --check <<< "$UV_SHA256  $RUNNER_TEMP/uv.tar.gz"
+  mkdir -p "$HOME/.local/bin"
+  tar -xf "$RUNNER_TEMP/uv.tar.gz" --strip-components=1 -C "$HOME/.local/bin"
 
   # Fix macdeployqt issue (https://github.com/actions/runner-images/issues/7522)
   echo "Killing XProtect..."
   sudo pkill -9 XProtect >/dev/null || true;
   while pgrep XProtect; do sleep 3; done;
 
-  # Add Qt to PATH
-  export PATH="$QT_ROOT/bin:$PATH"
+  # Add Qt & tools to PATH
+  export PATH="$QT_ROOT/bin:$HOME/.local/bin:$PATH"
 fi
